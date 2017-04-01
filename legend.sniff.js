bc02ebe60b356463c025959228406db2a458b545
jimboy3100 <jimboy3100@hotmail.com>
2017-04-01 09:47:35 +0300

    Update legend.sniff.js

--- legend.sniff.js
+++ legend.sniff.js
@@ -3,10 +3,9 @@
 var url = window.location.href;
 localStorage.setItem("url", url);
 
-var searchSip = getParameterByName("sip", url);
+
 		// fix party stuff for no SIP
-		if (searchSip==null){
- 	
+
    	$('#gamemode').on('change', function () {
       if (this.value == ":party") { $("#create-party-btn-2").click(); }
-		console.log( "Party stuff fixed" );});}
+		console.log( "Party stuff fixed" );})
