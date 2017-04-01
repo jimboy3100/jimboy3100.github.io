d134967712f55ab3503b7d1fe43b49e29867d25f
jimboy3100 <jimboy3100@hotmail.com>
2017-04-01 10:15:36 +0300

    Update legend.sniff.js

--- legend.sniff.js
+++ legend.sniff.js
@@ -6,6 +6,6 @@ localStorage.setItem("url", url);
 
 		// fix party stuff for no SIP
 
-   	$('#gamemode').on('change', function () {
-      if (this.value == ":party") { $("#create-party-btn-2").click(); }
-		console.log( "Party stuff fixed" );})
+//   	$('#gamemode').on('change', function () {
+//      if (this.value == ":party") { $("#create-party-btn-2").click(); }
+//		console.log( "Party stuff fixed" );})
