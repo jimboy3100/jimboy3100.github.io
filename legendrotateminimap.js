9da4bc47b3fb0951182d1770ed248223da6bede9
jimboy3100 <jimboy3100@hotmail.com>
2017-04-09 00:45:44 +0300

    Create legendrotateminimap.js

---
+++ legendrotateminimap.js
@@ -0,0 +1,172 @@
+var c=document.getElementById("minimap-sectors");var ctx = c.getContext("2d");ctx.clearRect(0, 0, c.width, c.height);
+ctx.font="16px Georgia";
+var xcanvas=$("#minimap-sectors").width();
+var spacewidth=($("#minimap").width() -  xcanvas)/2;
+var spaceheight=$("#minimap").height() -  xcanvas - spacewidth;
+
+
+//Var c=document.getElementById("minimap-sectors");
+//var ctx=c.getContext("2d");
+
+ctx.beginPath();
+ctx.moveTo(0,spaceheight);
+ctx.lineTo(0,$("#minimap").height() - spacewidth);
+ctx.lineTo(xcanvas,$("#minimap").height() - spacewidth);
+ctx.lineTo(xcanvas,spaceheight);
+ctx.lineTo(0,spaceheight);
+ctx.stroke();
+
+
+ctx.moveTo(xcanvas/5,spaceheight);
+ctx.lineTo(xcanvas/5,$("#minimap").height() - spacewidth);
+ctx.stroke();
+ctx.moveTo(2*xcanvas/5,spaceheight);
+ctx.lineTo(2*xcanvas/5,$("#minimap").height() - spacewidth);
+ctx.stroke();
+ctx.moveTo(3*xcanvas/5,spaceheight);
+ctx.lineTo(3*xcanvas/5,$("#minimap").height() - spacewidth);
+ctx.stroke();
+ctx.moveTo(4*xcanvas/5,spaceheight);
+ctx.lineTo(4*xcanvas/5,$("#minimap").height() - spacewidth);
+ctx.stroke();
+
+ctx.moveTo(0,spaceheight + xcanvas/5);
+ctx.lineTo(xcanvas,spaceheight + xcanvas/5);
+ctx.stroke();
+ctx.moveTo(0,spaceheight + 2*xcanvas/5);
+ctx.lineTo(xcanvas,spaceheight + 2*xcanvas/5);
+ctx.stroke();
+ctx.moveTo(0,spaceheight + 3*xcanvas/5);
+ctx.lineTo(xcanvas,spaceheight + 3*xcanvas/5);
+ctx.stroke();
+ctx.moveTo(0,spaceheight + 4*xcanvas/5);
+ctx.lineTo(xcanvas,spaceheight + 4*xcanvas/5);
+ctx.stroke();
+
+if (rotateminimap==0){
+ctx.fillText("A1",(xcanvas/5)/2,spaceheight+(xcanvas/5)/2);
+ctx.fillText("A2",3*(xcanvas/5)/2,spaceheight+(xcanvas/5)/2);
+ctx.fillText("A3",5*(xcanvas/5)/2,spaceheight+(xcanvas/5)/2);
+ctx.fillText("A4",7*(xcanvas/5)/2,spaceheight+(xcanvas/5)/2);
+ctx.fillText("A5",9*(xcanvas/5)/2,spaceheight+(xcanvas/5)/2);
+
+ctx.fillText("B1",(xcanvas/5)/2,spaceheight+3*(xcanvas/5)/2);
+ctx.fillText("B2",3*(xcanvas/5)/2,spaceheight+3*(xcanvas/5)/2);
+ctx.fillText("B3",5*(xcanvas/5)/2,spaceheight+3*(xcanvas/5)/2);
+ctx.fillText("B4",7*(xcanvas/5)/2,spaceheight+3*(xcanvas/5)/2);
+ctx.fillText("B5",9*(xcanvas/5)/2,spaceheight+3*(xcanvas/5)/2);
+
+ctx.fillText("C1",(xcanvas/5)/2,spaceheight+5*(xcanvas/5)/2);
+ctx.fillText("C2",3*(xcanvas/5)/2,spaceheight+5*(xcanvas/5)/2);
+ctx.fillText("C3",5*(xcanvas/5)/2,spaceheight+5*(xcanvas/5)/2);
+ctx.fillText("C4",7*(xcanvas/5)/2,spaceheight+5*(xcanvas/5)/2);
+ctx.fillText("C5",9*(xcanvas/5)/2,spaceheight+5*(xcanvas/5)/2);
+
+ctx.fillText("D1",(xcanvas/5)/2,spaceheight+7*(xcanvas/5)/2);
+ctx.fillText("D2",3*(xcanvas/5)/2,spaceheight+7*(xcanvas/5)/2);
+ctx.fillText("D3",5*(xcanvas/5)/2,spaceheight+7*(xcanvas/5)/2);
+ctx.fillText("D4",7*(xcanvas/5)/2,spaceheight+7*(xcanvas/5)/2);
+ctx.fillText("D5",9*(xcanvas/5)/2,spaceheight+7*(xcanvas/5)/2);
+
+ctx.fillText("E1",(xcanvas/5)/2,spaceheight+9*(xcanvas/5)/2);
+ctx.fillText("E2",3*(xcanvas/5)/2,spaceheight+9*(xcanvas/5)/2);
+ctx.fillText("E3",5*(xcanvas/5)/2,spaceheight+9*(xcanvas/5)/2);
+ctx.fillText("E4",7*(xcanvas/5)/2,spaceheight+9*(xcanvas/5)/2);
+ctx.fillText("E5",9*(xcanvas/5)/2,spaceheight+9*(xcanvas/5)/2);
+}
+//Rotate 90 degrees
+if (rotateminimap==1){
+ctx.fillText("E1",(xcanvas/5)/2,spaceheight+(xcanvas/5)/2);
+ctx.fillText("D1",3*(xcanvas/5)/2,spaceheight+(xcanvas/5)/2);
+ctx.fillText("C1",5*(xcanvas/5)/2,spaceheight+(xcanvas/5)/2);
+ctx.fillText("B1",7*(xcanvas/5)/2,spaceheight+(xcanvas/5)/2);
+ctx.fillText("A1",9*(xcanvas/5)/2,spaceheight+(xcanvas/5)/2);
+
+ctx.fillText("E2",(xcanvas/5)/2,spaceheight+3*(xcanvas/5)/2);
+ctx.fillText("D2",3*(xcanvas/5)/2,spaceheight+3*(xcanvas/5)/2);
+ctx.fillText("C2",5*(xcanvas/5)/2,spaceheight+3*(xcanvas/5)/2);
+ctx.fillText("B2",7*(xcanvas/5)/2,spaceheight+3*(xcanvas/5)/2);
+ctx.fillText("A2",9*(xcanvas/5)/2,spaceheight+3*(xcanvas/5)/2);
+
+ctx.fillText("E3",(xcanvas/5)/2,spaceheight+5*(xcanvas/5)/2);
+ctx.fillText("D3",3*(xcanvas/5)/2,spaceheight+5*(xcanvas/5)/2);
+ctx.fillText("C3",5*(xcanvas/5)/2,spaceheight+5*(xcanvas/5)/2);
+ctx.fillText("B3",7*(xcanvas/5)/2,spaceheight+5*(xcanvas/5)/2);
+ctx.fillText("A3",9*(xcanvas/5)/2,spaceheight+5*(xcanvas/5)/2);
+
+ctx.fillText("E4",(xcanvas/5)/2,spaceheight+7*(xcanvas/5)/2);
+ctx.fillText("D4",3*(xcanvas/5)/2,spaceheight+7*(xcanvas/5)/2);
+ctx.fillText("C4",5*(xcanvas/5)/2,spaceheight+7*(xcanvas/5)/2);
+ctx.fillText("B4",7*(xcanvas/5)/2,spaceheight+7*(xcanvas/5)/2);
+ctx.fillText("A4",9*(xcanvas/5)/2,spaceheight+7*(xcanvas/5)/2);
+
+ctx.fillText("E5",(xcanvas/5)/2,spaceheight+9*(xcanvas/5)/2);
+ctx.fillText("D5",3*(xcanvas/5)/2,spaceheight+9*(xcanvas/5)/2);
+ctx.fillText("C5",5*(xcanvas/5)/2,spaceheight+9*(xcanvas/5)/2);
+ctx.fillText("B5",7*(xcanvas/5)/2,spaceheight+9*(xcanvas/5)/2);
+ctx.fillText("A5",9*(xcanvas/5)/2,spaceheight+9*(xcanvas/5)/2);
+}
+//Rotate 180 degrees
+if (rotateminimap==2){
+ctx.fillText("E5",(xcanvas/5)/2,spaceheight+(xcanvas/5)/2);
+ctx.fillText("E4",3*(xcanvas/5)/2,spaceheight+(xcanvas/5)/2);
+ctx.fillText("E3",5*(xcanvas/5)/2,spaceheight+(xcanvas/5)/2);
+ctx.fillText("E2",7*(xcanvas/5)/2,spaceheight+(xcanvas/5)/2);
+ctx.fillText("E1",9*(xcanvas/5)/2,spaceheight+(xcanvas/5)/2);
+
+ctx.fillText("D5",(xcanvas/5)/2,spaceheight+3*(xcanvas/5)/2);
+ctx.fillText("D4",3*(xcanvas/5)/2,spaceheight+3*(xcanvas/5)/2);
+ctx.fillText("D3",5*(xcanvas/5)/2,spaceheight+3*(xcanvas/5)/2);
+ctx.fillText("D2",7*(xcanvas/5)/2,spaceheight+3*(xcanvas/5)/2);
+ctx.fillText("D1",9*(xcanvas/5)/2,spaceheight+3*(xcanvas/5)/2);
+
+ctx.fillText("C5",(xcanvas/5)/2,spaceheight+5*(xcanvas/5)/2);
+ctx.fillText("C4",3*(xcanvas/5)/2,spaceheight+5*(xcanvas/5)/2);
+ctx.fillText("C3",5*(xcanvas/5)/2,spaceheight+5*(xcanvas/5)/2);
+ctx.fillText("C2",7*(xcanvas/5)/2,spaceheight+5*(xcanvas/5)/2);
+ctx.fillText("C1",9*(xcanvas/5)/2,spaceheight+5*(xcanvas/5)/2);
+
+ctx.fillText("B5",(xcanvas/5)/2,spaceheight+7*(xcanvas/5)/2);
+ctx.fillText("B4",3*(xcanvas/5)/2,spaceheight+7*(xcanvas/5)/2);
+ctx.fillText("B3",5*(xcanvas/5)/2,spaceheight+7*(xcanvas/5)/2);
+ctx.fillText("B2",7*(xcanvas/5)/2,spaceheight+7*(xcanvas/5)/2);
+ctx.fillText("B1",9*(xcanvas/5)/2,spaceheight+7*(xcanvas/5)/2);
+
+ctx.fillText("A5",(xcanvas/5)/2,spaceheight+9*(xcanvas/5)/2);
+ctx.fillText("A4",3*(xcanvas/5)/2,spaceheight+9*(xcanvas/5)/2);
+ctx.fillText("A3",5*(xcanvas/5)/2,spaceheight+9*(xcanvas/5)/2);
+ctx.fillText("A2",7*(xcanvas/5)/2,spaceheight+9*(xcanvas/5)/2);
+ctx.fillText("A1",9*(xcanvas/5)/2,spaceheight+9*(xcanvas/5)/2);
+}
+//Rotate 270 degrees
+if (rotateminimap==3){
+ctx.fillText("A5",(xcanvas/5)/2,spaceheight+(xcanvas/5)/2);
+ctx.fillText("B5",3*(xcanvas/5)/2,spaceheight+(xcanvas/5)/2);
+ctx.fillText("C5",5*(xcanvas/5)/2,spaceheight+(xcanvas/5)/2);
+ctx.fillText("D5",7*(xcanvas/5)/2,spaceheight+(xcanvas/5)/2);
+ctx.fillText("E5",9*(xcanvas/5)/2,spaceheight+(xcanvas/5)/2);
+
+ctx.fillText("A4",(xcanvas/5)/2,spaceheight+3*(xcanvas/5)/2);
+ctx.fillText("B4",3*(xcanvas/5)/2,spaceheight+3*(xcanvas/5)/2);
+ctx.fillText("C4",5*(xcanvas/5)/2,spaceheight+3*(xcanvas/5)/2);
+ctx.fillText("D4",7*(xcanvas/5)/2,spaceheight+3*(xcanvas/5)/2);
+ctx.fillText("E4",9*(xcanvas/5)/2,spaceheight+3*(xcanvas/5)/2);
+
+ctx.fillText("A3",(xcanvas/5)/2,spaceheight+5*(xcanvas/5)/2);
+ctx.fillText("B3",3*(xcanvas/5)/2,spaceheight+5*(xcanvas/5)/2);
+ctx.fillText("C3",5*(xcanvas/5)/2,spaceheight+5*(xcanvas/5)/2);
+ctx.fillText("D3",7*(xcanvas/5)/2,spaceheight+5*(xcanvas/5)/2);
+ctx.fillText("E3",9*(xcanvas/5)/2,spaceheight+5*(xcanvas/5)/2);
+
+ctx.fillText("A2",(xcanvas/5)/2,spaceheight+7*(xcanvas/5)/2);
+ctx.fillText("B2",3*(xcanvas/5)/2,spaceheight+7*(xcanvas/5)/2);
+ctx.fillText("C2",5*(xcanvas/5)/2,spaceheight+7*(xcanvas/5)/2);
+ctx.fillText("D2",7*(xcanvas/5)/2,spaceheight+7*(xcanvas/5)/2);
+ctx.fillText("E2",9*(xcanvas/5)/2,spaceheight+7*(xcanvas/5)/2);
+
+ctx.fillText("A1",(xcanvas/5)/2,spaceheight+9*(xcanvas/5)/2);
+ctx.fillText("B1",3*(xcanvas/5)/2,spaceheight+9*(xcanvas/5)/2);
+ctx.fillText("C1",5*(xcanvas/5)/2,spaceheight+9*(xcanvas/5)/2);
+ctx.fillText("D1",7*(xcanvas/5)/2,spaceheight+9*(xcanvas/5)/2);
+ctx.fillText("E1",9*(xcanvas/5)/2,spaceheight+9*(xcanvas/5)/2);
+}
