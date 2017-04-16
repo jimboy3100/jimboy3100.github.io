86579919d7c93a283fe6d246013023d0865c4811
jimboy3100 <jimboy3100@hotmail.com>
2017-04-17 00:54:52 +0300

    Update legend.sniff.js

--- legend.sniff.js
+++ legend.sniff.js
@@ -5,6 +5,11 @@ localStorage.setItem("url", url);
 
 
 		// fix party stuff for no SIP
+	// ANNOUNCEMENTS
+	if(modVersion!="2.0"){ toastr["error"]('Mod v' + modVersion + ' can be Updated to v2.0, visit <a target="_blank" href="https://github.com/jimboy3100/legend.github.io/raw/master/legendmod.user.js">www.legendmod.ml</a>');}
+	//else{toastr["info"]('Hello ' + tag1 +'! </br>Legend Mod v' + modVersion + ' website: <a target="_blank" href="http://www.legendmod.ml/">LINK</a>');
+	else{toastr["info"]('Welcome back ' + tag1 + '!');
+	toastr["info"]('1. Rejoin with token for communication to activate on FFA/EXP. 2. If bug occurs, chrome://settings/clearBrowserData delete cookies');}
 
 //   	$('#gamemode').on('change', function () {
 //      if (this.value == ":party") { $("#create-party-btn-2").click(); }
