dbdb464f94db2ed69c59dde0ae5b3caf53c29d90
Dimitrios Kyriakidis <jimboy3100@hotmail.com>
2017-06-22 17:39:46 +0300

    Update dyinglight.js

--- dyinglight.js
+++ dyinglight.js
@@ -1,4 +1,5 @@
-
+//Dying Light Extension For Legend Mod
+//By Jimboy3100
 
 if(dyinglight1load==null||dyinglight1load=="null"){
 toastr["warning"]('<div id="tutorial" style="background-image: url(https://jimboy3100.github.io/banners/dyinglightbanner.jpg); color:#018cf6; font-size:16px; text-align:center">Are you sure you want to load Dying Light Template?<br>Save your Settings on a file, because they will be lost.<br>' + '</br> <button class="btn btn-sm btn-primary btn-play btn-do-DyingLight" style="margin-top: 10px;border-color: darkblue;">' + Premadeletter24 + '</button><br><button class="btn btn-sm btn-warning btn-spectate btn-nodo-hideall" style="width: 100%;margin-top: 10px;">'+ Premadeletter25 + '</button></div>', "", { timeOut: 20000, extendedTimeOut: 20000 }).css("width", "300px");	
@@ -119,9 +120,9 @@ function stopdyinglight(){
 
 
 MC.onPlayerDeath=function(){ 
-$("#canvas").css('background-image', 'url(https://i.imgur.com/5jIjJVc.gif)');		
+$("#minimap").css('background-image', 'url(https://i.imgur.com/5jIjJVc.gif)');		
 setTimeout(function () {
-$("#canvas").css('background-image', 'url(https://jimboy3100.github.io/banners/dyinglightbabyteamboard.png)');		
+$("#minimap").css('background-image', 'url(https://jimboy3100.github.io/banners/dyinglightbabyteamboard.png)');		
 
 
 }, 20000);
