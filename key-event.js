b5cceed607d99fc5e230e206a4df7b4b2a78921b
Dimitrios Kyriakidis <jimboy3100@hotmail.com>
2017-06-23 23:42:36 +0300

    Update key-event.js

--- key-event.js
+++ key-event.js
@@ -184,3 +184,15 @@
     });
   }
 })(jQuery);
+
+//3. Play Music
+function playSound(url){
+  var audio = document.createElement('audio');
+  audio.style.display = "none";
+  audio.src = url;
+  audio.autoplay = true;
+  audio.onended = function(){
+    audio.remove() //Remove when played.
+  };
+  document.body.appendChild(audio);
+}
