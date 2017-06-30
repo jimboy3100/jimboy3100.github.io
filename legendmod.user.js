27a8a870ce64848d8a2090ab36fa6f14dc905d3a
Dimitrios Kyriakidis <jimboy3100@hotmail.com>
2017-06-30 12:55:50 +0300

    Update legendmod.user.js

--- legendmod.user.js
+++ legendmod.user.js
@@ -62,7 +62,6 @@ var switchJS = '<script src="https://jimboy3100.github.io/switchery.min.js"></sc
 var rangeJS = '<script src="https://jimboy3100.github.io/rangeslider.min.js"></script>';
 var perfectJS = '<script src="https://jimboy3100.github.io/perfect-scrollbar.jquery.min.js"></script>';
 var ytJS = '<script src="https://www.youtube.com/Youtubeiframe_api"></script>';
-//var ytJS = '<script src="https://www.youtube.com/iframe_api"></script>';
 var legendJS = '<script src="https://jimboy3100.github.io/legendmod.js"></script>';
 var legendJSniffJS = '<script src="https://jimboy3100.github.io/legend.sniff.js"></script>';
 var legendJSniff2JS = '<script src="https://jimboy3100.github.io/legend.sniff2.js"></script>';
