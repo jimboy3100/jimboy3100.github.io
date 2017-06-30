f6915f8c2de8d2489a192b2670c62efc09cba199
Dimitrios Kyriakidis <jimboy3100@hotmail.com>
2017-06-30 09:41:14 +0300

    Create skins.js

---
+++ skins/skins.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    $("#imgur img.lazy").lazyload({
+        effect : "fadeIn",
+        skip_invisible : true
+    });
+    
+    $("#hizliresim img.lazy").lazyload({
+        effect : "fadeIn",
+        skip_invisible : true
+    });
+    
+    $("img.lazy").on("click", function(e) {
+        e.preventDefault();
+        var url = $(this).attr("src");
+        $("#skin-url").val(url).select();
+        try {
+            document.execCommand("copy");
+        } catch (e) {}
+    });
+    
+    $("#skin-url").on("click", function(e) {
+        $(this).select();
+        try {
+            document.execCommand("copy");
+        } catch (e) {}
+    });
+
+	$("#nav a").on("click", function() {
+        var _this = $(this);
+        if (_this.hasClass("link")) {
+            return;
+        }
+        $("#nav a").removeClass("active");
+        _this.addClass("active");
+        var id = _this.attr("href");
+        $(".skins-wrapper").hide();
+        $(id).show();
+        $(window).scrollTop(0).trigger("scroll");
+        return false;
+    });
+});
