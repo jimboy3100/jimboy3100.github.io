7f1c1efcbcef1175f23bbe1d08a1d7a04bc1ecd4
Dimitrios Kyriakidis <jimboy3100@hotmail.com>
2017-07-18 06:13:16 +0300

    Update legendmod.user.js

--- legendmod.user.js
+++ legendmod.user.js
@@ -5,6 +5,7 @@
 // @description  Agario Mod - Legend,Ogario,Kitty,Old Skins,Animated Skins,Language Packs,Manual User Scripts,Chat,60++ Macros/Hotkeys(Tricksplit,Doublesplit,Quick Feeding,Popsplit,Auto Coins,Freeze Cell Macro,Auto respawn)
 // @homepage     http://www.legendmod.ml
 // @author       Jimboy3100
+// @license      MIT
 // @icon         https://jimboy3100.github.io/banners/CropedImage128.gif
 // @match        http://agar.io/*
 // @match        https://talky.io/*
