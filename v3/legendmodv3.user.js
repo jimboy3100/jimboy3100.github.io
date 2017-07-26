aed7b5fb8b3d6a2a64c6bc62a6d681f3ac3162a4
Dimitrios Kyriakidis <jimboy3100@hotmail.com>
2017-07-26 14:36:04 +0300

    Update legendmodv3.user.js

--- v3/legendmodv3.user.js
+++ v3/legendmodv3.user.js
@@ -1,16 +1,25 @@
 // ==UserScript==
-// @name         OGARio by szymy v4 Beta
-// @namespace    ogario.v4.b
-// @version      4.0.0
-// @description  Unoffical Polish MOD
-// @author       szymy
+// @name         Legend Agar.io Extension Tempory Edition
+// @namespace    Legend Agar.io
+// @version      2.2
+// @description  Agario Mod - Legend,Ogario,Kitty,Old Skins,Animated Skins,Language Packs,Manual User Scripts,Chat,60++ Macros/Hotkeys(Tricksplit,Doublesplit,Quick Feeding,Popsplit,Auto Coins,Freeze Cell Macro,Auto respawn)
+// @homepage     http://www.legendmod.ml
+// @author       Jimboy3100
+// @license      MIT
+// @icon         https://jimboy3100.github.io/banners/CropedImage128.gif
 // @match        http://agar.io/*
+// @match        https://talky.io/*
+// @match        https://play.google.com/*
+// @downloadURL  jimboy3100.github.io/legendmod.user.js
+// @updateURL    jimboy3100.github.io/legendmod.user.js
 // @run-at       document-start
 // @grant        GM_xmlhttpRequest
-// @connect      cdn.ogario.ovh
+// @grant GM_setValue
+// @grant GM_getValue
+// @grant GM_deleteValue
 // ==/UserScript==
-
-// © 2017 ogario.ovh
+// Legend Mod by Jimboy3100
+/*MIT License
 
 // Check location
 //if (location.host === "agar.io" && location.pathname === "/") {
