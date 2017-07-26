84c148c45f3c26521c5c0d651c65bbc6c0c15ccd
Dimitrios Kyriakidis <jimboy3100@hotmail.com>
2017-07-26 19:07:55 +0300

    Update v3.user.js

--- v3.user.js
+++ v3.user.js
@@ -65,7 +65,7 @@ setTimeout(function() {
         document.getElementsByClassName('quick-yt icon-youtube2')[0].setAttribute('data-original-title', 'Legend Promo Video');
         $('#legendid').addClass('fa fa-thumbs-o-up').removeClass('quick-yt icon-youtube2');
 		$("#more-skins").attr('href', 'https://jimboy3100.github.io/skins/skins.html');
-	
+		$("#leaderboard-hud > h4").text("Leaderboard");
 		$("#___ytsubscribe_0").remove();
 
 
