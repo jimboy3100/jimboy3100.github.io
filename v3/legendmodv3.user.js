79b5054c9c2fd5f466eb3b760da48d0c6fb2f5b6
Dimitrios Kyriakidis <jimboy3100@hotmail.com>
2017-07-26 14:57:40 +0300

    Update legendmodv3.user.js

--- v3/legendmodv3.user.js
+++ v3/legendmodv3.user.js
@@ -60,5 +60,55 @@ if (location.host == "agar.io" && location.pathname == "/") {
 		}
 	});
 }
+// Inject Chat Talky.io Userscript
+if (location.host == "talky.io") {
+	
+	(function() {
+    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = 'https://jimboy3100.github.io/banners/icon32croped.ico.gif';
+    document.getElementsByTagName('head')[0].appendChild(link);
+	})();
+
+	document.title="Legend Mod - Talky";
+	
+    var url2 = window.location.href;
+    var gamename = getParameterByName("name", url2);
+    var IPAgario = getParameterByName("ip", url2);
+
+    setTimeout(function() {
+        document.getElementsByClassName('_26dP_7FWLFRnvW8hs-AIzR')[0].remove();
+        document.getElementsByClassName('_26dP_7FWLFRnvW8hs-AIzR')[0].remove();
+        document.getElementsByClassName('_26dP_7FWLFRnvW8hs-AIzR')[0].remove();
+    }, 4100);
+
+    setTimeout(function() {
+        document.getElementById('skip').click();
+        document.getElementsByClassName('TalkyButton__text')[0].click();
+
+    }, 3500);
+    setTimeout(function() {
+        document.getElementsByClassName('_1U4l9qYTHl6ExTsW9IvwnO')[1].value = gamename;
+        //<a href="http://legendmod.ml" target="_blank" id="LegendModWebsite" class="title" style=""><u>Legend Mod</u></a> <a href=IPAgario target="_blank" id="IPAgario" class="title" style=""><u>Copy Agar.io Token</u></a> 
+        
+		if (IPAgario.length==6){
+		document.getElementsByClassName('message message-info message-full-width')[0].before("[Talky.io]: Legend Mod. Server: " + "http://agar.io/#" + IPAgario + " . [PARTY] (Password rooms are different than Public)");}
+		else{
+		document.getElementsByClassName('message message-info message-full-width')[0].before("[Talky.io]: Legend Mod. Server: " + "http://agar.io/?sip=" + IPAgario + " . (Password rooms are different than Public)");}	
+		
+        document.getElementsByClassName('_1U4l9qYTHl6ExTsW9IvwnO')[1].value = gamename;
+        document.getElementsByClassName('message message-info message-full-width')[0].style.visibility = 'hidden';
+        document.getElementsByClassName('Box _3-HLfCQ5QT5fuKgw4tvBkP')[0].remove();
+        document.getElementsByClassName('-ZMXacQm9s80kTx3I-A47')[0].remove();
+
+    }, 4000);
+}
+//runs only if play.google.com is a popup, doesnt if directly joined
+if (location.host == "play.google.com") {
+	window.close();
+}
+
 
 //End of Legend Mod
+
