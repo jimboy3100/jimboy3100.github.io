3585456c1f2eea70c75e0964358d83d7f2df6ed6
Dimitrios Kyriakidis <jimboy3100@hotmail.com>
2017-07-26 18:39:23 +0300

    Update v3.user.js

--- v3.user.js
+++ v3.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Legend Agar.io Extension Temporary Edition
+// @name         Legend Agar.io Extension Tempory Edition
 // @namespace    Legend Agar.io
 // @version      2.2
 // @description  Agario Mod - Legend,Ogario,Kitty,Old Skins,Animated Skins,Language Packs,Manual User Scripts,Chat,60++ Macros/Hotkeys(Tricksplit,Doublesplit,Quick Feeding,Popsplit,Auto Coins,Freeze Cell Macro,Auto respawn)
@@ -55,6 +55,22 @@ GM_xmlhttpRequest({
         document.close();
     }
 });
+setTimeout(function() {
+        var ogarioVersion = $("#menu-footer").text().split("| ")[1];
+        $("#menu-footer").text("");
+		$("#menu-footer").prepend('<span style="float: left; font-size: 13px;"><a target="_blank" onclick="ga(\'send\', \'event\', \'Link\', \'click\', \'legendWebsite\');" href="http://www.legendmod.ml" style="color: #ffffff;" data-toggle="tooltip" data-title="Legend Mod Website" data-placement="left">Legend mod ' + 'Temporary Script (BUGGY)' + '</a></span>' +
+            '<a href="https://legendmod.joomla.com/en/more-fps.html" data-toggle="tooltip" data-title="How to improve performance" data-placement="top" style ="font-size: 13px"; target="_blank">More FPS</a>');
+        document.getElementsByClassName('quick-yt icon-youtube2')[0].setAttribute('id', 'legendid');
+        document.getElementsByClassName('quick-yt icon-youtube2')[0].href = "https://www.youtube.com/watch?v=CnIfNSpCf70";
+        document.getElementsByClassName('quick-yt icon-youtube2')[0].setAttribute('data-original-title', 'Legend Promo Video');
+        $('#legendid').addClass('fa fa-thumbs-o-up').removeClass('quick-yt icon-youtube2');
+		$("#more-skins").attr('href', 'https://jimboy3100.github.io/skins/skins.html');
+	
+		$("#___ytsubscribe_0").remove();
+setTimeout(function() {		
+	toastr["warning"]("This Version is temporary and buggy. Install the old one when its ready");		
+	}, 5000);
+}, 2000);    
 }
 
 
