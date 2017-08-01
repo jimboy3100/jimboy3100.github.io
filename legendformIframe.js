d588e9bef40c0720b2c50b4cba45123450451c90
Dimitrios Kyriakidis <jimboy3100@hotmail.com>
2017-08-01 13:08:01 +0300

    Update legendformIframe.js

--- legendformIframe.js
+++ legendformIframe.js
@@ -9,7 +9,7 @@ $("#menu-footer").after('<div id="legendformIframe" style="background-image: url
 	'</iframe>'	+
 											'</div></div>'+
 											    '<button id= "Backtomenu" onclick="closecustomskinsIframe(); return false" style="margin-left: 10px;" class="btn btn-danger"  data-original-title="" title="">CLOSE</button>'+
-												'<button id= "SendAFileform" onclick="SendAFileform(); return false" style="margin-left: 10px;" class="btn btn-success"  data-original-title="" title="">SEND CODE TO DEVELOPER</button></div>');
+												'<button id= "SendAFileform" onclick="SendAFileform(); return false" style="margin-left: 10px;" class="btn btn-success"  data-original-title="" title="">SEND CODE TO DEVELOPERS</button></div>');
 
 
 									
