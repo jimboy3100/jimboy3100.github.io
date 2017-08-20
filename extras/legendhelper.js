161f74419711173c9ac750fb1912cd8d9330475d
Dimitrios Kyriakidis <jimboy3100@hotmail.com>
2017-08-21 00:48:24 +0300

    Update legendhelper.js

--- extras/legendhelper.js
+++ extras/legendhelper.js
@@ -3,9 +3,9 @@ $("#main-menu").hide();
 $("#skins-panel").hide();
 $("#quick-menu").hide();
 $("#exp-bar").hide();
-$("#menu-footer").after('<div id="legendhelper" style="background-image: url('+legbgpic+'); background-color: '+legbgcolor+'; border: 1px solid black; position:absolute;  height: 560px; width: 920px; margin-top: 0px;margin-left: -240px; ">'+
+$("#menu-footer").after('<div id="legendhelper" style="background-image: url('+legbgpic+'); background-color: '+legbgcolor+'; border: 1px solid black; position:absolute;  height: 560px; width: 940px; margin-top: 0px;margin-left: -240px; ">'+
 	'<div id="Userscriptshud2" style="display:block; margin-left: 10px; margin-right: 10px;">'+ //margin-left: 10px"
-	'<br><div id="Userscriptshud3"><iframe id="customskinsIframe2" src="https://jimboy3100.github.io/extras/helper.html" width="900" height="490" >'+
+	'<br><div id="Userscriptshud3"><iframe id="customskinsIframe2" src="https://jimboy3100.github.io/extras/helper.html" width="920" height="490" >'+
     '<p>Your browser does not support iframes.</p>'+
 	'</iframe>'	+
 											'</div></div>'+
