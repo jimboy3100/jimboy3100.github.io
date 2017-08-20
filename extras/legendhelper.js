f31270b1234ed2fcd7fc6468a3b6a25604d27841
Dimitrios Kyriakidis <jimboy3100@hotmail.com>
2017-08-21 00:45:21 +0300

    Update legendhelper.js

--- extras/legendhelper.js
+++ extras/legendhelper.js
@@ -5,7 +5,7 @@ $("#quick-menu").hide();
 $("#exp-bar").hide();
 $("#menu-footer").after('<div id="legendhelper" style="background-image: url('+legbgpic+'); background-color: '+legbgcolor+'; border: 1px solid black; position:absolute;  height: 560px; width: 920px; margin-top: 0px;margin-left: -240px; ">'+
 	'<div id="Userscriptshud2" style="display:block; margin-left: 10px; margin-right: 10px;">'+ //margin-left: 10px"
-	'<br><div id="Userscriptshud3"><iframe id="customskinsIframe2" src="https://jimboy3100.github.io/helper.html" width="900" height="490" >'+
+	'<br><div id="Userscriptshud3"><iframe id="customskinsIframe2" src="https://jimboy3100.github.io/extras/helper.html" width="900" height="490" >'+
     '<p>Your browser does not support iframes.</p>'+
 	'</iframe>'	+
 											'</div></div>'+
