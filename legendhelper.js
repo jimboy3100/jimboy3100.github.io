26d353b149270d3f34f31e6953f058e8a5bf61a9
Dimitrios Kyriakidis <jimboy3100@hotmail.com>
2017-08-21 01:00:58 +0300

    Update legendhelper.js

--- legendhelper.js
+++ legendhelper.js
@@ -3,9 +3,9 @@ $("#main-menu").hide();
 $("#skins-panel").hide();
 $("#quick-menu").hide();
 $("#exp-bar").hide();
-$("#menu-footer").after('<div id="legendhelper" style="background-image: url('+legbgpic+'); background-color: '+legbgcolor+'; border: 1px solid black; position:absolute;  height: 560px; width: 840px; margin-top: 0px;margin-left: -240px; ">'+
+$("#menu-footer").after('<div id="legendhelper" style="background-image: url('+legbgpic+'); background-color: '+legbgcolor+'; border: 1px solid black; position:absolute;  height: 560px; width: 940px; margin-top: 0px;margin-left: -240px; ">'+
 	'<div id="Userscriptshud2" style="display:block; margin-left: 10px; margin-right: 10px;">'+ //margin-left: 10px"
-	'<br><div id="Userscriptshud3"><iframe id="customskinsIframe2" src="https://jimboy3100.github.io/helper.html" width="820" height="490" >'+
+	'<br><div id="Userscriptshud3"><iframe id="customskinsIframe2" src="https://jimboy3100.github.io/extras/helper.html" width="920" height="490" >'+
     '<p>Your browser does not support iframes.</p>'+
 	'</iframe>'	+
 											'</div></div>'+
@@ -19,7 +19,7 @@ $("#main-menu").show();
 $("#skins-panel").show();
 $("#quick-menu").show();
 $("#exp-bar").show();
-$("#legendhelper").hide();
+$("#legendhelper").remove();
 }
 function templatesIframe(){
 
