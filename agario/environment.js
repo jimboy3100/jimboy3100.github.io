19dd21df244a09d541ded5204a1591ab3fab1d9a
Dimitrios Kyriakidis <jimboy3100@hotmail.com>
2017-09-13 20:41:23 +0300

    Create environment.js

---
+++ agario/environment.js
@@ -0,0 +1,18 @@
+window.EnvConfig = {
+    "env_production": true,
+    "fb_app_id": 677505792353827,
+    "gplus_client_id": "686981379285-oroivr8u2ag1dtm3ntcs6vi05i3cpv0j.apps.googleusercontent.com",
+    "master_url": "m.agar.io",
+    "xsolla_endpoint": "https://payments.agario.miniclippt.com",
+    "fb_endpoint": "https://rewardcallback-live.agario.miniclippt.com/payments/facebook",
+    "game_url": "http://agar.io",
+    "supersonic_app_key": "416358b5",
+    "tap_research_api_key": "f2b4fd7d3dd1a7d871d79cdb1330c5ef",
+    "config_url": "https://configs-web.agario.miniclippt.com/live/v7",
+    "configVersion": "7",
+    "apiKey": "1001d717-309f-11e7-b7a2-0633a732f6c7",
+    "checksumKey": "100308e4-309f-11e7-b7a2-0633a732f6c7",
+    "analyticsEnv": "production",
+    NR_licenseKey: "f246896502",
+    NR_applicationID: "18400599"
+}
