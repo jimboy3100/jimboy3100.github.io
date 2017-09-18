cea5f86c82df263a89545e1443e57dd0fa75d5ff
Legend Mod <jimboy3100@hotmail.com>
2017-09-18 23:56:06 +0300

    Update environment.js

--- agario/environment.js
+++ agario/environment.js
@@ -2,7 +2,7 @@ window.EnvConfig = {
     "env_production": true,
     "fb_app_id": 677505792353827,
     "gplus_client_id": "686981379285-oroivr8u2ag1dtm3ntcs6vi05i3cpv0j.apps.googleusercontent.com",
-    "master_url": "m.agar.io",
+    "master_url": "bouncer-v4.agar.io",
     "xsolla_endpoint": "https://payments.agario.miniclippt.com",
     "fb_endpoint": "https://rewardcallback-live.agario.miniclippt.com/payments/facebook",
     "game_url": "http://agar.io",
