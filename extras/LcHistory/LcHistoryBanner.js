9114da1f3fca1988d055b70bcf3466ea92c9b0f5
Legend Mod <jimboy3100@hotmail.com>
2017-11-10 02:34:52 +0200

    Create LcHistoryBanner.js

---
+++ extras/LcHistory/LcHistoryBanner.js
@@ -0,0 +1,21 @@
+$("#main-menu").hide();
+$("#skins-panel").hide();
+$("#quick-menu").hide();
+$("#exp-bar").hide();
+$("#menu-footer").after('<div id="legendhelper" style="background-image: url('+legbgpic+'); background-color: '+legbgcolor+'; border: 1px solid black; position:absolute;  height: 560px; width: 940px; margin-top: 0px;margin-left: -240px; ">'+
+	'<div id="Userscriptshud2" style="display:block; margin-left: 10px; margin-right: 10px;">'+ //margin-left: 10px"
+	'<br><div id="Userscriptshud3"><iframe id="customskinsIframe2" src="https://jimboy3100.github.io/extras/LcHistory/LcHistory.html" width="920" height="490" >'+
+    '<p>Your browser does not support iframes.</p>'+
+	'</iframe>'	+
+											'</div></div>'+
+											    '<button id= "Backtomenu" onclick="closecustomskinsIframe(); return false" style="margin-left: 10px;" class="btn btn-danger"  data-original-title="" title="">CLOSE</button>');
+
+
+									
+function closecustomskinsIframe(){
+$("#main-menu").show();	
+$("#skins-panel").show();
+$("#quick-menu").show();
+$("#exp-bar").show();
+$("#legendhelper").remove();
+}
