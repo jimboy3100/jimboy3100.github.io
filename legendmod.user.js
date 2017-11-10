7ba0ff91a7eb9d2d4a07412296a53f0608e4ba86
Legend Mod <jimboy3100@hotmail.com>
2017-11-10 16:37:54 +0200

    Update legendmod.user.js

--- legendmod.user.js
+++ legendmod.user.js
@@ -46,6 +46,15 @@ SOFTWARE.
 */
 
 // Start of script
+GM_registerMenuCommand('Legend Mod Website', function() {
+    window.open("https://legendmod.joomla.com/en/");
+}, 'r');
+GM_registerMenuCommand('LM Library', function() {
+    window.open("https://github.com/jimboy3100/jimboy3100.github.io/");
+}, 'r');
+GM_registerMenuCommand('Donate for Legend Mod', function() {
+    window.open("https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=CM3GDVCW6PBF6");
+}, 'r');
 if (location.host === "agar.io" && location.pathname === "/") {
 	window.stop();	
 	location.href = "about:blank" + window.location.search + location.hash;
