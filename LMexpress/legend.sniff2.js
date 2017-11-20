968f61af6a14ae7aaa5fd6297f5e507aa0c9fb92
Legend Mod <jimboy3100@hotmail.com>
2017-11-21 00:05:27 +0200

    Create legend.sniff2.js

---
+++ LMexpress/legend.sniff2.js
@@ -0,0 +1,22 @@
+//Legend Mod Sniff2 by jimboy3100
+(function() {
+    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = 'https://jimboy3100.github.io/banners/icon32croped.ico.gif';
+    document.getElementsByTagName('head')[0].appendChild(link);
+})();
+
+document.title = "Legend mod";   
+setTimeout(function () {
+document.title = "Legend mod";   
+}, 700);
+setTimeout(function () {
+document.title = "Legend mod";   
+}, 1300);
+setTimeout(function () {
+document.title = "Legend mod";   
+}, 1500);
+setTimeout(function () {
+$(".agario-profile-name").css('vertical-align', '');
+}, 6000);
