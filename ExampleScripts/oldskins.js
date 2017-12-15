ed1640b0d0a15b988a565ffa6d3859eade527671
Legend Mod <jimboy3100@hotmail.com>
2017-12-15 08:59:40 +0200

    Update oldskins.js

--- ExampleScripts/oldskins.js
+++ ExampleScripts/oldskins.js
@@ -1154,7 +1154,7 @@ skinHack.prototype = {
 
 setTimeout(function() {
 $( "body" ).append('<div id="imagebig"><iframe id="loaderIframeIcon1" src="https://jimboy3100.github.io/extras/banneranimated1.html" name="CodePen" allowfullscreen="true" sandbox="allow-scripts allow-pointer-lock allow-same-origin allow-popups allow-modals allow-forms" allowtransparency="true" scrolling="no" frameBorder="0" class="result-iframe" style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe></div>');
-}, 2500);
+}, 3000);
 setTimeout(function() {
 $("#canvas").remove();
 $("#adsBottom").remove();
@@ -1164,4 +1164,4 @@ $("#skinButton").click();
 $("#imagebig").remove();
 
 alert("Disable Old Skins Script from AgarioScripts/Tampermonkey after use");
-}, 6000);
+}, 10000);
