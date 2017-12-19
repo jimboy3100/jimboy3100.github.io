b24fdb720ee2814c70274784c1096b978b3cb911
Legend Mod <jimboy3100@hotmail.com>
2017-12-19 14:26:50 +0200

    Create core2.js

---
+++ old/core2.js
@@ -0,0 +1,9 @@
+//MAP AUTO START
+CanvasRenderingContext2D.prototype.drawImage = CanvasRenderingContext2D.prototype.drawImage, CanvasRenderingContext2D.prototype.drawImage = function() {
+  if (arguments[0].src) {
+    if ("http://agar.io/img/background.png" == arguments[0].src) {
+      arguments[0].src = "";
+    }
+  }
+  this.drawImage.apply(this, arguments);
+};
