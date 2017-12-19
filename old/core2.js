5ad0270021fb6419b254ba735f3f0302dea67b24
Legend Mod <jimboy3100@hotmail.com>
2017-12-19 14:56:47 +0200

    Update core2.js

--- old/core2.js
+++ old/core2.js
@@ -1,10 +1,9 @@
 //MAP AUTO START
-
-CanvasRenderingContext2D.prototype.drawImage = CanvasRenderingContext2D.prototype.drawImage, CanvasRenderingContext2D.prototype.drawImage = function() {
+CanvasRenderingContext2D.prototype._drawImage = CanvasRenderingContext2D.prototype.drawImage, CanvasRenderingContext2D.prototype.drawImage = function() {
   if (arguments[0].src) {
     if ("http://agar.io/img/background.png" == arguments[0].src) {
       arguments[0].src = "";
     }
   }
-  this.drawImage.apply(this, arguments);
+  this._drawImage.apply(this, arguments);
 };
