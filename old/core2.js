2e542527894a1047467e553a0d391d75ffe817fe
Legend Mod <jimboy3100@hotmail.com>
2017-12-19 14:28:58 +0200

    Update core2.js

--- old/core2.js
+++ old/core2.js
@@ -1,4 +1,5 @@
 //MAP AUTO START
+
 CanvasRenderingContext2D.prototype.drawImage = CanvasRenderingContext2D.prototype.drawImage, CanvasRenderingContext2D.prototype.drawImage = function() {
   if (arguments[0].src) {
     if ("http://agar.io/img/background.png" == arguments[0].src) {
