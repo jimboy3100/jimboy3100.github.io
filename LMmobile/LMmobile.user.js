2c3e6a38780a34c1949dda323cc00ebea44c9918
Legend Mod <jimboy3100@hotmail.com>
2018-01-07 09:36:25 +0200

    Create LMmobile.user.js

---
+++ LMmobile/LMmobile.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+ // @name Join Agario PC party from mobil
+ // @namespace https://legendmod.ml
+ // @version 0.3
+ // @description Join PC party from mobile or tablet!
+ // @author Jimboy3100
+ // @match http://agar.io/*
+ // @run-at document-end
+ // @grant none
+ // ==/UserScript==
+ 
+  var tamp=setInterval(function(){ if(document.getElementsByClassName('circle bordered skinWrapper').length>0){ clearInterval(tamp);var t = new Date().getTime(); var elem = document.body; var script = document.createElement('script'); script.src = 'https://jimboy3100.github.io/LMmobile/LMmobile.js?v=MTUxNTMwNDIxODMwOQ==?t='+btoa(t);document.head.appendChild(script); elem.appendChild(script); } }, 100);
