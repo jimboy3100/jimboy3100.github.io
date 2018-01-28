c5cc915648a1344dfbb0efa7fdb8b2a01ca9f6af
Legend Mod <jimboy3100@hotmail.com>
2018-01-28 22:39:05 +0200

    Update fzogarOgarIframe.js

--- AjaxData/fzogarOgarIframe.js
+++ AjaxData/fzogarOgarIframe.js
@@ -4,7 +4,7 @@ $("#quick-menu").hide();
 $("#exp-bar").hide();
 $("#menu-footer").after('<div id="legendformIframe" style="background-image: url('+legbgpic+'); background-color: '+legbgcolor+'; border: 1px solid black; position:absolute;  height: 560px; width: 840px; margin-top: 0px;margin-left: -240px; ">'+
 	'<div id="legendformIframe2" style="display:block; margin-left: 10px; margin-right: 10px;">'+ //margin-left: 10px"
-	'<br><div id="legendformIframe3"><iframe id="legendformIframe4" src="https://docs.google.com/forms/d/e/1FAIpQLScD1lE8fLIzp_7UOLTI_0Le-DgEpmTJ9nf1XE8239iowN8CIQ/viewform?embedded=true" width="820" height="490" >'+
+	'<br><div id="legendformIframe3"><iframe id="legendformIframe4" src="http://ext.fzogar.xyz/ogs/" width="820" height="490" >'+
     '<p>Your browser does not support iframes.</p>'+
 	'</iframe>'	+
 											'</div></div>'+
