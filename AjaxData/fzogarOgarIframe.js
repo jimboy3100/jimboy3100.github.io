6d65fd7fcadca325c3c867202efac1cb41a23d2c
Legend Mod <jimboy3100@hotmail.com>
2018-01-29 04:44:45 +0200

    Update fzogarOgarIframe.js

--- AjaxData/fzogarOgarIframe.js
+++ AjaxData/fzogarOgarIframe.js
@@ -2,13 +2,14 @@ $("#main-menu").hide();
 $("#skins-panel").hide();
 $("#quick-menu").hide();
 $("#exp-bar").hide();
+$("close-exp-imp").click();
 $("#menu-footer").after('<div id="legendformIframe" style="background-image: url('+legbgpic+'); background-color: '+legbgcolor+'; border: 1px solid black; position:absolute;  height: 700px; width: 640px; margin-top: 0px;margin-left: -240px; ">'+
 	'<div id="legendformIframe2" style="display:block; margin-left: 10px; margin-right: 10px;">'+ //margin-left: 10px"
 	'<br><div id="legendformIframe3"><iframe id="legendformIframe4" src="http://ext.fzogar.xyz/ogs/" width="620" height="630" >'+
     '<p>Your browser does not support iframes.</p>'+
 	'</iframe>'	+
 											'</div></div>'+
-											    '<button id= "Backtomenu" onclick="closecustomskinsIframe(); return false" style="margin-left: 10px;" class="btn btn-danger"  data-original-title="" title="">CLOSE</button></div>'+
+											    '<button id= "Backtomenu" onclick="closecustomskinsIframe(); return false" style="margin-left: 10px;" class="btn btn-danger"  data-original-title="" title="">CLOSE</button>'+
 												'<button id= "CopyExportedFromAgario" onclick="CopyExportedFromAgario(); return false" style="margin-left: 10px;" class="btn btn-success"  data-original-title="" title="">COPY FROM AGARIO</button></div>');
 
 
