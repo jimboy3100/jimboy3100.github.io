49c921bbd3ee543ae970cb33594cc33293006fa4
Legend Mod <jimboy3100@hotmail.com>
2018-01-30 06:24:52 +0200

    Delete fzogarOgarIframeSniff.js

--- AjaxData/fzogarOgarIframeSniff.js
+++
@@ -1,8 +0,0 @@
-var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
-        var eventer = window[eventMethod];
-        var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
-        eventer(messageEvent, function (e) {
-       if (~e.data.indexOf("PostedOgarSettings2")){
-	   alert(e.data);
-	   }
-        }, false);
