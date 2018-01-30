9c0ac1b8d9a6bb98052840e1135315cc5c0b7078
Legend Mod <jimboy3100@hotmail.com>
2018-01-30 08:11:48 +0200

    Update fzogarOgarIframe.user.js

--- AjaxData/fzogarOgarIframe.user.js
+++ AjaxData/fzogarOgarIframe.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Fzogar Upload / Download Ogario Settings
+// @name         Fzogar Upload / Download OGARio Settings
 // @namespace    Upload your settings from agario to webpage or apply them on mod easily
 // @version      1
 // @description  Upload your JSON file settings from agario to webpage or apply them on mod easily
@@ -57,44 +57,13 @@ if (location.href == "http://ext.fzogar.xyz/ogs/") {
 }
 //runs for Agario settings
 if (location.host == "agar.io") {
-setTimeout(function (){ 	
+var messageone;
 if ($("#clantag").val()!=undefined){ //If Using Ogario, Kitty Mod
 if (messageone!="0"||messageone!="1"){	//If Using Legend Mod
-var datasent2;
-
-var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
-var eventer = window[eventMethod];
-
-//fzogar Upload / Download Settings
-$("#import-settings-btn").attr('class', 'btn btn-success');
-$("#close-exp-imp").before('<button id="fzogarOgarBtn" onclick="fzogarOgarIframe(); return false" style="margin-right: 25px;" class="btn btn-success" data-original-title="" title="">Upload / Download</button>');
-
-
-var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
-eventer(messageEvent, function (e) {
-	   if (~e.data.indexOf("PostedOgarSettings1")){ 
-		datasent2=e.data;
-		datasent2=datasent2.replace("PostedOgarSettings1?datasent=", "");
-		fzogarOgarSettings(datasent2);   
-	   }
-		return datasent2;
-}, false);
-}
-}
-}, 5000);
-}
-
-function fzogarOgarSettings(datasent2){
-        setTimeout(function() {
-            $("#import-settings-btn").attr('class', 'btn btn-success');
-            document.getElementById("import-settings").value = datasent2;
-            window.history.pushState(null, null, window.location.pathname);
-            $('#import-settings-btn').click();
-        }, 100);
-}		
-function fzogarOgarIframe() {
     var s = document.createElement("script");
     s.type = "text/javascript";
-    s.src = "https://jimboy3100.github.io/AjaxData/fzogarOgarIframe.js";
+    s.src = "https://jimboy3100.github.io/AjaxData/fzogarOgarIframeSniff.js";
     $("body").append(s);
+}
+}
 }
