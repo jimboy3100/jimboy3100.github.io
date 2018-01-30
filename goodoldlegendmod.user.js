70b2e904836b2b53e0dc7cb7b1ae965ef24c00fc
Legend Mod <jimboy3100@hotmail.com>
2018-01-30 08:57:12 +0200

    Update goodoldlegendmod.user.js

--- goodoldlegendmod.user.js
+++ goodoldlegendmod.user.js
@@ -28,7 +28,7 @@
 
 /*MIT License
 
-Copyright (c) [2017] [The Legend Mod]
+Copyright (c) [The Legend Mod]
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
