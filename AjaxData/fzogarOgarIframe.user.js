21f51fff9f6c1994fc1678b5addbd3f6eb401e31
Legend Mod <jimboy3100@hotmail.com>
2018-01-30 07:43:59 +0200

    Create fzogarOgarIframe.user.js

---
+++ AjaxData/fzogarOgarIframe.user.js
@@ -0,0 +1,100 @@
+// ==UserScript==
+// @name         Fzogar Upload / Download Ogario Settings
+// @namespace    Upload your settings from agario to webpage or apply them on mod easily
+// @version      1
+// @description  Upload your JSON file settings from agario to webpage or apply them on mod easily
+// @homepage     http://www.legendmod.ml
+// @author       Jimboy3100
+// @license      MIT
+// @icon         https://jimboy3100.github.io/banners/CropedImage128.gif
+// @match        http://agar.io/*
+// @match        http://ext.fzogar.xyz/ogs/*
+// @downloadURL  jimboy3100.github.io/legendmod.user.js
+// @updateURL    jimboy3100.github.io/legendmod.user.js
+// @run-at       document-start
+// @grant        GM_xmlhttpRequest
+// @connect      jimboy3100.github.io
+// ==/UserScript==
+
+
+// Website: http://ext.fzogar.xyz/ogs/
+// Legend Mod by Jimboy3100 for Fayiz
+// Part of the Legend Mod Project Agario http://www.legendmod.ml
+
+/*MIT License
+Copyright (c) [The Legend Mod]
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+//runs only for http://ext.fzogar.xyz/ogs settings
+if (location.href == "http://ext.fzogar.xyz/ogs/") {
+    setTimeout(function() {
+    $("#login_form").append('<span style="float: left; font-size: 13px;">Powered by <a target="_blank" href="http://ext.fzogar.xyz/ogs/" style="color: #ffffff;" data-toggle="tooltip" data-title="Legend mod Website" data-placement="left"><u>http://ext.fzogar.xyz/ogs/</u></a></span>');
+    $("#upload-button").after('<input type="submit" id="sendInfo" class="btn btn-default " value="Apply Settings to Mod" style="margin-left: 7px;">');
+//    $("#sendInfo").after('<input type="submit" id="sendInfo2" class="btn btn-default " value="Upload from Legend Mod" style="margin-left: 7px;">');
+    $("#sendInfo").click(function() {
+        showpaste();
+//        alert($("#jsonupdate").val());
+        try{window.parent.postMessage("PostedOgarSettings1?datasent="+$("#jsonupdate").val(), "*");
+        } catch (e) {}
+    });
+
+        }, 1100);
+}
+//runs for Agario settings
+if (location.host == "agar.io") {
+setTimeout(function (){ 	
+if ($("#clantag").val()!=undefined){ //If Using Ogario, Kitty Mod
+if (messageone!="0"||messageone!="1"){	//If Using Legend Mod
+var datasent2;
+
+var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
+var eventer = window[eventMethod];
+
+//fzogar Upload / Download Settings
+$("#import-settings-btn").attr('class', 'btn btn-success');
+$("#close-exp-imp").before('<button id="fzogarOgarBtn" onclick="fzogarOgarIframe(); return false" style="margin-right: 25px;" class="btn btn-success" data-original-title="" title="">Upload / Download</button>');
+
+
+var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
+eventer(messageEvent, function (e) {
+	   if (~e.data.indexOf("PostedOgarSettings1")){ 
+		datasent2=e.data;
+		datasent2=datasent2.replace("PostedOgarSettings1?datasent=", "");
+		fzogarOgarSettings(datasent2);   
+	   }
+		return datasent2;
+}, false);
+}
+}
+}, 5000);
+}
+
+function fzogarOgarSettings(datasent2){
+        setTimeout(function() {
+            $("#import-settings-btn").attr('class', 'btn btn-success');
+            document.getElementById("import-settings").value = datasent2;
+            window.history.pushState(null, null, window.location.pathname);
+            $('#import-settings-btn').click();
+        }, 100);
+}		
+function fzogarOgarIframe() {
+    var s = document.createElement("script");
+    s.type = "text/javascript";
+    s.src = "https://jimboy3100.github.io/AjaxData/fzogarOgarIframe.js";
+    $("body").append(s);
+}
