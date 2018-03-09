b4e74a2d9df7d0a79531cb8b5aa5360b8d638301
Legend Mod <jimboy3100@hotmail.com>
2018-03-10 01:06:05 +0200

    Update extension.js

--- extension/extension.js
+++ extension/extension.js
@@ -31,7 +31,6 @@ if (!window.AgarToolLoaded) {
                 dispatchEvent(new Event("load")), times++, detectExtensions(), times > 400 && clearInterval(myInterval)
             }, 50);
 
-var modVersion= "3.2";
 var cpickerCSS = '<link href="https://jimboy3100.github.io/bootstrap-colorpicker.min.css" rel="stylesheet"></link>';
 var toastrCSS = '<link href="https://jimboy3100.github.io/toastr.min.css" rel="stylesheet"></link>';
 var switchCSS = '<link href="https://jimboy3100.github.io/switchery.min.css" rel="stylesheet"></link>';
@@ -40,7 +39,6 @@ var perfectCSS = '<link href="https://jimboy3100.github.io/perfect-scrollbar.min
 var faCSS = '<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"></link>';
 var legendarioCSS = '<link href="http://cdn.ogario.ovh/v3/ogario.v3.css?v=339" rel="stylesheet"></link>';
 
-//var ytJS = '<script src="https://jimboy3100.github.io/Youtubeiframe_api.js"></script>';
 var keyJS = '<script src="https://jimboy3100.github.io/key-event.js"></script>';
 var cpickerJS = '<script src="https://jimboy3100.github.io/bootstrap-colorpicker.min.js"></script>';
 var toastrJS = '<script src="https://jimboy3100.github.io/toastr.min.js"></script>';
@@ -53,16 +51,39 @@ var legendJSniff2JS = '<script src="https://jimboy3100.github.io/legend.sniff2.j
 var legendJSniff3JS = '<script src="https://jimboy3100.github.io/legend.sniff3.js"></script>';
 var legendarioSniffJS = '<script src="http://cdn.ogario.ovh/v3/ogario.v3.sniff.js?v=339"></script>';
 var legendarioJS = '<script src="http://cdn.ogario.ovh/v3/ogario.v3.js?v=339" charset="utf-8"></script>';
-			
-	// loadScript("https://jimboy3100.github.io/extension/loadCSS.js", function() {	
-	
-        //    document.write("</head>", legendarioSniffJS + cpickerCSS + toastrCSS + switchCSS + rangeCSS + perfectCSS + legendarioCSS + faCSS + cpickerJS + toastrJS + switchJS + rangeJS + perfectJS + keyJS + legendJSniff2JS + "</head>");
-       //     document.write("</body>", legendJSniffJS + "</body");
-       //     setTimeout(function() {
-       //     document.write("</body>", legendarioJS + legendJS + legendJSniff3JS +  "<script>init('" + modVersion + "');</script>" + "</body>");
 
-       //     document.close();
-         //       }, 1500);
-	//})
+var modVersion = "3.2";
+
+
+function reqListener () {
+    var doc=this.responseText;
+    window.stop();
+    document.documentElement.innerHTML = "";
+    document.open();
+    document.write(doc);
+  //console.log(this.responseText);
+            document.write("</head>", legendarioSniffJS + cpickerCSS + toastrCSS + switchCSS + rangeCSS + perfectCSS + legendarioCSS + faCSS + cpickerJS + toastrJS + switchJS + rangeJS + perfectJS + keyJS + legendJSniff2JS + "</head>");
+            document.write("</body>", legendJSniffJS + "</body");
+            setTimeout(function() {
+            document.write("</body>", legendarioJS + legendJS + legendJSniff3JS +  "<script>init('" + modVersion + "');</script>" + "</body>");
+
+            document.close();
+                }, 1500);
+}
+
+var oReq = new XMLHttpRequest();
+oReq.addEventListener("load", reqListener);
+oReq.open("GET", "http://agar.io");
+oReq.send();
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
     }
 }
