c35702fc21754deaa3e8c1db0b322a6c60042120
Legend Mod <jimboy3100@hotmail.com>
2018-04-27 01:59:18 +0300

    Update LMmozzila.user.js

--- ExampleScripts/LMmozzila.user.js
+++ ExampleScripts/LMmozzila.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @name         Legend Agar.io Extension
-// @namespace    Legend Agario Mod
+// @name         Legend Agar.io Extension (Mozzila)
+// @namespace    Legend Agario Mod (Mozzila)
 // @version      3.1
 // @description  Agario Mod - Legend,Ogario,Kitty,Old Skins,Animated Skins,Language Packs,Manual User Scripts,Chat,60++ Macros/Hotkeys(Tricksplit,Doublesplit,Quick Feeding,Popsplit,Auto Coins,Freeze Cell Macro,Auto respawn)
 // @author       Jimboy3100
@@ -9,10 +9,10 @@
 // ==/UserScript==	
 // Legend Mod by Jimboy3100
 
-if (location.host == "agar.io" && location.pathname == "/") {
-    location.href = "http://agar.io/ogario" + location.hash;
-    return;
-}
+var used;
+LM();
+function LM(){
+if (used==null){
 			var script = document.createElement("script");
 			script.type = "text/javascript";
 			script.src = location.protocol + "//jimboy3100.github.io/extension/extension.js?ts="+Date.now();
@@ -21,3 +21,6 @@ if (location.host == "agar.io" && location.pathname == "/") {
 				setTimeout(loadExtension, 100);
 			};
 			document.head.appendChild(script);
+return used=1;			
+}
+}
