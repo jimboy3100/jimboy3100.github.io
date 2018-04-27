23bb5dd28c92853657c2042c54f529e107e80c4d
Legend Mod <jimboy3100@hotmail.com>
2018-04-27 18:38:13 +0300

    Update libLM.user.js

--- ExampleScripts/libLM.user.js
+++ ExampleScripts/libLM.user.js
@@ -4,7 +4,7 @@
 // @version      0.4
 // @description  try to take over the world!
 // @author       You
-// @match        http://www.un.org/en/about-un/
+// @match        http://www.atmarkit.co.jp/d9KcsYjB
 // @grant        unsafeWindow
 // ==/UserScript==
 
