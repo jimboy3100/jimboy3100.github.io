b0337981565d77784f51733c1fe94193bbbe66d1
Legend Mod <jimboy3100@hotmail.com>
2019-01-22 06:34:24 +0200

    Update LanguagePackEnglish.js

--- LanguagePackEnglish.js
+++ LanguagePackEnglish.js
@@ -448,8 +448,8 @@ var Premadeletter44="Show XP BAR";		//Show XP BAR
 var Premadeletter45="Hide XP BAR";		//Hide XP BAR
 var Premadeletter45a = "Rounded Hud"; //Rounded Hud
 var Premadeletter45b = "Square Hud"; //Square Hud
-var Premadeletter46 = "Show Anim. Skins"; //Show Anim. Skins
-var Premadeletter47 = "Hide Anim. Skins"; //Hide Anim. Skins
+var Premadeletter46 = "Lin. color Hud"; //Linear color Hud
+var Premadeletter47 = "Hide Anim. Skins"; //Bold color Hud
 
 var Premadeletter48="Show Everything";		//Show Everything
 var Premadeletter49="Hide Everything";		//Hide Everything
