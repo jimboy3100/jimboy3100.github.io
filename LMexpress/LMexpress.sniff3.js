3f4bfaa2557f59a73b20c809fb4ee40474cb7d0e
Legend Mod <jimboy3100@hotmail.com>
2019-02-08 20:05:30 +0200

    Update LMexpress.sniff3.js

--- LMexpress/LMexpress.sniff3.js
+++ LMexpress/LMexpress.sniff3.js
@@ -1,1 +1,10 @@
+//Legend Mod Sniff 3 by jimboy3100
 
+//1. RegisterSkins
+if ( typeof core != 'undefined'){
+//core.registerSkin('fly', null, 'https://i.imgur.com/poFMdZd.png', 1, null);
+	core.registerSkin = function(a, b, c, d, e){
+	window.customskinsname=a;
+	window.customskinsurl=c;
+	}
+}
