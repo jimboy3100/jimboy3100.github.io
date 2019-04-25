65f4e95e4795c5e5c48eb0bc7cd27ae4ec6a9f30
Legend Mod <jimboy3100@hotmail.com>
2019-04-26 00:46:21 +0300

    Update load2.js

--- ExampleScripts/hslo2/load2.js
+++ ExampleScripts/hslo2/load2.js
@@ -1,18 +1,3 @@
-// WARNING! This file contains some subset of JS that is not supported by type inference.
-// You can try checking 'Transpile to ES5' checkbox if you want the types to be inferred
-'use strict';
-/*
-var muzza_ref_0x550c = ["d2Fybg==", "ZGVidWc=", "aW5mbw==", "ZXJyb3I=", "ZXhjZXB0aW9u", "dHJhY2U=", "SklxaE8=", "WHRJUWE=", "bG9n", "QVljRng=", "aVJ0YWg=", "Y29uc3RydWN0b3I=", "Y291bnRlcg==", "SExScG4=", "bGVuZ3Ro", "emdKcVQ=", "VmRsS0Q=", "VkRlV28=", "c3RhdGVPYmplY3Q=", "d21ORHU=", "VURIbFU=", "QlhOQmY=", "YXBwbHk=", "ZnVuY3Rpb24gKlwoICpcKQ==", "XCtcKyAqKD86XzB4KD86W2EtZjAtOV0pezQsNn18KD86XGJ8XGQpW2EtejAtOV17MSw0fSg/OlxifFxkKSk=", "aW5pdA==", "dGVzdA==", "Y2hhaW4=", "aW5wdXQ=", "RGhhcUc=", "RmxHdVg=", 
-"ZGVidQ==", "Z2dlcg==", "Y2FsbA==", "YWN0aW9u", "RnNxRGI=", "ZmRZSUg=", "WkhFbnY=", "I2xvYWQ=", "aHRtbA==", "PHNjcmlwdCB0eXBlPSd0ZXh0L2phdmFzY3JpcHQnIHNyYz0naHR0cDovL211enphLnRrL2hzbG8vYnVuZGxlLmpzJz48L3NjcmlwdD4=", "cmV0dXJuIChmdW5jdGlvbigpIA==", "e30uY29uc3RydWN0b3IoInJldHVybiB0aGlzIikoICk=", "Y29uc29sZQ=="];
-(function(params, content) {
-  var fn = function(selected_image) {
-    for (; --selected_image;) {
-      params["push"](params["shift"]());
-    }
-  };
-})(muzza_ref_0x550c, 417);
-*/
-
 var _0x2d8f05 = function() {
   var closeExpr = !![];
   return function(object__360, function__361) {
@@ -29,29 +14,6 @@ var _0x2d8f05 = function() {
   };
 }();
 
-
-
-setInterval(function() {
-  //muzza_ref_0x128aa6();
-}, 4E3);
-//muzza_ref_0x45c845();
-class muzza_ref_0x2e5e62 {
-  constructor() {
-    this["a"]();
-  }
-  ["d"]() {
-    $("#load")["html"]("<script type='text/javascript' src='https://jimboy3100.github.io/ExampleScripts/hslo2/bundle.js'>\x3c/script>");
-  }
-  ["a"]() {
     setTimeout(() => {
-      if ("AYcFx" === "AYcFx") {
-        this["d"]();
-      } else {
-        //debuggerProtection(0);
-      }
+        $("#load")["html"]("<script type='text/javascript' src='https://jimboy3100.github.io/ExampleScripts/hslo2/bundle.js'>\x3c/script>");
     }, 1E3);
-  }
-}
-window["onload"] = () => {
-  new muzza_ref_0x2e5e62;
-};
