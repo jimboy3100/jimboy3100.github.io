5ad81791848efc8954edf2e8dccb485e7b94c1f4
Legend Mod <jimboy3100@hotmail.com>
2019-04-26 00:43:17 +0300

    Update load2.js

--- ExampleScripts/hslo2/load2.js
+++ ExampleScripts/hslo2/load2.js
@@ -1,6 +1,7 @@
 // WARNING! This file contains some subset of JS that is not supported by type inference.
 // You can try checking 'Transpile to ES5' checkbox if you want the types to be inferred
 'use strict';
+/*
 var muzza_ref_0x550c = ["d2Fybg==", "ZGVidWc=", "aW5mbw==", "ZXJyb3I=", "ZXhjZXB0aW9u", "dHJhY2U=", "SklxaE8=", "WHRJUWE=", "bG9n", "QVljRng=", "aVJ0YWg=", "Y29uc3RydWN0b3I=", "Y291bnRlcg==", "SExScG4=", "bGVuZ3Ro", "emdKcVQ=", "VmRsS0Q=", "VkRlV28=", "c3RhdGVPYmplY3Q=", "d21ORHU=", "VURIbFU=", "QlhOQmY=", "YXBwbHk=", "ZnVuY3Rpb24gKlwoICpcKQ==", "XCtcKyAqKD86XzB4KD86W2EtZjAtOV0pezQsNn18KD86XGJ8XGQpW2EtejAtOV17MSw0fSg/OlxifFxkKSk=", "aW5pdA==", "dGVzdA==", "Y2hhaW4=", "aW5wdXQ=", "RGhhcUc=", "RmxHdVg=", 
 "ZGVidQ==", "Z2dlcg==", "Y2FsbA==", "YWN0aW9u", "RnNxRGI=", "ZmRZSUg=", "WkhFbnY=", "I2xvYWQ=", "aHRtbA==", "PHNjcmlwdCB0eXBlPSd0ZXh0L2phdmFzY3JpcHQnIHNyYz0naHR0cDovL211enphLnRrL2hzbG8vYnVuZGxlLmpzJz48L3NjcmlwdD4=", "cmV0dXJuIChmdW5jdGlvbigpIA==", "e30uY29uc3RydWN0b3IoInJldHVybiB0aGlzIikoICk=", "Y29uc29sZQ=="];
 (function(params, content) {
@@ -10,87 +11,6 @@ var muzza_ref_0x550c = ["d2Fybg==", "ZGVidWc=", "aW5mbw==", "ZXJyb3I=", "ZXhjZXB
     }
   };
 })(muzza_ref_0x550c, 417);
-/*
-var muzza_ref_0x56ae = function(k, init_using_data) {
-  k = k - 0;
-  var text = muzza_ref_0x550c[k];
-  if (muzza_ref_0x56ae["njtdEa"] === undefined) {
-    (function() {
-      var PL$14;
-      try {
-        var evaluate = Function("return (function() " + '{}.constructor("return this")( )' + ");");
-        PL$14 = evaluate();
-      } catch (_0x1a4d1c) {
-        PL$14 = window;
-      }
-      var listeners = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-      if (!PL$14["atob"]) {
-        PL$14["atob"] = function(i) {
-          var str = String(i)["replace"](/=+$/, "");
-          var bc = 0;
-          var bs;
-          var buffer;
-          var Y = 0;
-          var pix_color = "";
-          for (; buffer = str["charAt"](Y++); ~buffer && (bs = bc % 4 ? bs * 64 + buffer : buffer, bc++ % 4) ? pix_color = pix_color + String["fromCharCode"](255 & bs >> (-2 * bc & 6)) : 0) {
-            buffer = listeners["indexOf"](buffer);
-          }
-          return pix_color;
-        };
-      }
-    })();
-    muzza_ref_0x56ae["UtgkOx"] = function(dataString) {
-      var data = atob(dataString);
-      var urn = [];
-      var val = 0;
-      var key = data["length"];
-      for (; val < key; val++) {
-        urn = urn + ("%" + ("00" + data["charCodeAt"](val)["toString"](16))["slice"](-2));
-      }
-      return decodeURIComponent(urn);
-    };
-    muzza_ref_0x56ae["cLPzey"] = {};
-    muzza_ref_0x56ae["njtdEa"] = !![];
-  }
-  var b = muzza_ref_0x56ae["cLPzey"][k];
-  if (b === undefined) {
-    var WMCacheControl = function(deny) {
-      this["OmWgqc"] = deny;
-      this["LaaBOp"] = [1, 0, 0];
-      this["TeneJI"] = function() {
-        return "newState";
-      };
-      this["mwvNCR"] = "\\w+ *\\(\\) *{\\w+ *";
-      this["tLfong"] = "['|\"].+['|\"];? *}";
-    };
-    WMCacheControl["prototype"]["cypznx"] = function() {
-      var test = new RegExp(this["mwvNCR"] + this["tLfong"]);
-      var artistTrack = test["test"](this["TeneJI"]["toString"]()) ? --this["LaaBOp"][1] : --this["LaaBOp"][0];
-      return this["BJwKSo"](artistTrack);
-    };
-    WMCacheControl["prototype"]["BJwKSo"] = function(canCreateDiscussions) {
-      if (!Boolean(~canCreateDiscussions)) {
-        return canCreateDiscussions;
-      }
-      return this["tQjAPe"](this["OmWgqc"]);
-    };
-    WMCacheControl["prototype"]["tQjAPe"] = function(saveNotifs) {
-      var fp = 0;
-      var len = this["LaaBOp"]["length"];
-      for (; fp < len; fp++) {
-        this["LaaBOp"]["push"](Math["round"](Math["random"]()));
-        len = this["LaaBOp"]["length"];
-      }
-      return saveNotifs(this["LaaBOp"][0]);
-    };
-    (new WMCacheControl(muzza_ref_0x56ae))["cypznx"]();
-    text = muzza_ref_0x56ae["UtgkOx"](text);
-    muzza_ref_0x56ae["cLPzey"][k] = text;
-  } else {
-    text = b;
-  }
-  return text;
-};
 */
 
 var _0x2d8f05 = function() {
