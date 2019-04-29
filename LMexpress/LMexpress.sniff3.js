31e0a4307c2431cf64ff359fd8cdac4a9a74947b
Legend Mod <jimboy3100@hotmail.com>
2019-04-30 02:20:45 +0300

    Update LMexpress.sniff3.js

--- LMexpress/LMexpress.sniff3.js
+++ LMexpress/LMexpress.sniff3.js
@@ -1,4 +1,15 @@
-    // Inject Chat to text userscript
+//Legend Mod Sniff 3 by jimboy3100
+
+//1. RegisterSkins
+if ( typeof core != 'undefined'){
+//core.registerSkin('fly', null, 'https://i.imgur.com/poFMdZd.png', 1, null);
+	core.registerSkin = function(a, b, c, d, e){
+	window.customskinsname=a;
+	window.customskinsurl=c;
+	}
+}
+
+// Inject Chat to text userscript
 setTimeout(function() {
 	console.log("VoiceDeOChat:Stage 1");
  var myEle = document.getElementById("voice-prefix");
