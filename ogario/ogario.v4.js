b1f4c575436d57fea3a9cb7686af3ef8ec5e0e0f
Legend Mod <jimboy3100@hotmail.com>
2019-05-12 04:47:43 +0300

    Update ogario.v4.js

--- ogario/ogario.v4.js
+++ ogario/ogario.v4.js
@@ -1,7 +1,7 @@
 // Open Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko
 // This is part of the Legend mod project
-// v1.328b MEGA TEST
+// v1.328c MEGA TEST
 // Game Configurations
 
 Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
@@ -84,7 +84,7 @@ function checkVideos1(a){
 	if (!videoJustWatchProflag[a]){
 		console.log("video skins activated");
 		window.videoJustWatchPro = document.createElement("video"); // create a video element
-		window.videoJustWatchPro.crossOrigin = 'anonymous';
+//		window.videoJustWatchPro.crossOrigin = 'anonymous';
 		window.videoJustWatchPro.src = a;	
 		window.videoJustWatchProflag[a]=true;
 	}
