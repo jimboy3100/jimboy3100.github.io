d082115292f8ca0a47959aec8696863ddbf5686a
Legend Mod <jimboy3100@hotmail.com>
2019-05-21 01:38:28 +0300

    Delete 4.js

--- ogario/4.js
+++
@@ -1,6 +0,0 @@
-$(window).on('load', function () {
-    var $preloader = $('#loading-screen'),
-        $svg_anm   = $preloader.find('.svg_load');
-    $svg_anm.fadeOut();
-    $preloader.delay(500).fadeOut('slow');
-});
