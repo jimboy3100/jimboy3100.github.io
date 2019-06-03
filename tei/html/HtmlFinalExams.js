457723389da3bef00039e148b7ca622c8b1f87ca
Legend Mod <jimboy3100@hotmail.com>
2019-06-04 00:35:22 +0300

    Create HtmlFinalExams.js

---
+++ tei/html/HtmlFinalExams.js
@@ -0,0 +1,18 @@
+//for my practice
+
+emoticons = new Array();
+emoticons[1] = {
+  name:'joy',
+  photourl:"https://jimboy3100.github.io/banners/emoticons/joy.svg",
+  price:'1',
+}
+emoticons[2] = {
+  name:'cry',
+  photourl:"https://jimboy3100.github.io/banners/emoticons/cry.svg",
+  price:'5',
+}
+emoticons[2] = {
+  name:'cool',
+  photourl:"https://jimboy3100.github.io/banners/emoticons/cool.svg",
+  price:'100',
+}
