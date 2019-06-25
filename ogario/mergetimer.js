fa6dc956cef04c03be24c97a0edf836250740a2a
Legend Mod <jimboy3100@hotmail.com>
2019-06-25 22:57:39 +0300

    Update mergetimer.js

--- ogario/mergetimer.js
+++ ogario/mergetimer.js
@@ -1,46 +1,45 @@
-//Time Merger v1
-window.ExternalScripts=true;
-var Intervalstatistics = setInterval(CellTimer, 1000);	
+//Time Merger v1.1
+window.ExternalScripts = true;
+var Intervalstatistics = setInterval(CellTimer, 1000);
+
 function CellTimer() {
-	if (!window.legendmod5.optimizedMass && window.ExternalScripts){
-	myCells = [];
-	for (var i = 0; i < window.legendmod.playerCells.length; i++) {
-//legendmod.cells[i].historyMass->window.playerCellsId[legendmod.cells[i].id].historyMass
-		
-			if (window.playerCellsId && window.legendmod.playerCells[i] && window.legendmod.playerCells[i].id && window.playerCellsId[legendmod.playerCells[i].id] && 
-			window.playerCellsId[legendmod.playerCells[i].id].historyMass && 
-			window.playerCellsId[legendmod.playerCells[i].id].historyMass[2*window.legendmod2.fps]>window.playerCellsId[legendmod.playerCells[i].id].historyMass[0]*1.4){
-				var j=i;
-				var x=0;
-					
-					MergeCells(j,x);
+    if (!window.legendmod5.optimizedMass && window.ExternalScripts) {
+        myCells = [];
+        for (var i = 0; i < window.legendmod.playerCells.length; i++) {
+            //legendmod.cells[i].historyMass->window.playerCellsId[legendmod.cells[i].id].historyMass
 
-			}
-			else{
-				if(window.playerCellsId && window.legendmod.playerCells[i] && window.legendmod.playerCells[i].id && window.playerCellsId[legendmod.playerCells[i].id] &&
-				window.playerCellsId[legendmod.playerCells[i].id].historyMass){
-				window.playerCellsId[legendmod.playerCells[i].id].mergeTime=null;
-			}
-			}
-			
-		}
-}
-	}	
+            if (window.playerCellsId && window.legendmod.playerCells[i] && window.legendmod.playerCells[i].id && window.playerCellsId[legendmod.playerCells[i].id] &&
+                window.playerCellsId[legendmod.playerCells[i].id].historyMass &&
+                window.playerCellsId[legendmod.playerCells[i].id].historyMass[2 * window.legendmod2.fps] > window.playerCellsId[legendmod.playerCells[i].id].historyMass[0] * 1.4) {
+                var j = i;
+                var x = 0;
 
-function MergeCells(j,x){
-x++;
-if (window.playerCellsId && window.legendmod.playerCells[j] && window.legendmod.playerCells[j].id && window.playerCellsId[legendmod.playerCells[j].id]){
-window.playerCellsId[legendmod.playerCells[j].id].mergeTime = 29 + (7/300)*window.playerCellsId[legendmod.playerCells[j].id].historyMass[0] - x;
-	//console.log(j,x, Math.round(window.playerCellsId[legendmod.playerCells[j].id].mergeTime));
-	if (window.playerCellsId[legendmod.playerCells[j].id].mergeTime>0){
-	setTimeout(function() {	
-		MergeCells(j,x);
-		}, 1000);
-	}
-	else{
-		//window.playerCellsId[legendmod.playerCells[j].id].mergeTime=null;
-	}
-}	
-	return j,x;
+                MergeCells(j, x);
+
+            } else {
+                if (window.playerCellsId && window.legendmod.playerCells[i] && window.legendmod.playerCells[i].id && window.playerCellsId[legendmod.playerCells[i].id] &&
+                    window.playerCellsId[legendmod.playerCells[i].id].historyMass) {
+                    window.playerCellsId[legendmod.playerCells[i].id].mergeTime = null;
+                }
+            }
+
+        }
+    }
 }
 
+function MergeCells(j, x) {
+    x++;
+    if (window.playerCellsId && window.legendmod.playerCells[j] && window.legendmod.playerCells[j].id && window.playerCellsId[legendmod.playerCells[j].id]) {
+        window.playerCellsId[legendmod.playerCells[j].id].mergeTime = 29 + (7 / 300) * window.playerCellsId[legendmod.playerCells[j].id].historyMass[0] - x;
+        //console.log(j,x, Math.round(window.playerCellsId[legendmod.playerCells[j].id].mergeTime));
+        if (window.playerCellsId[legendmod.playerCells[j].id].mergeTime > 0) {
+            setTimeout(function() {
+                MergeCells(j, x);
+            }, 1000);
+        }
+
+    } else {
+        window.playerCellsId[legendmod.playerCells[j].id].mergeTime = null;
+    }
+    return j, x;
+}
