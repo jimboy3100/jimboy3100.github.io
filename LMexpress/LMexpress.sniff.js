0157bee5e4beb697649850f5d360a137d03669d9
Legend Mod <jimboy3100@hotmail.com>
2019-06-27 05:52:52 +0300

    Update LMexpress.sniff.js

--- LMexpress/LMexpress.sniff.js
+++ LMexpress/LMexpress.sniff.js
@@ -22,7 +22,7 @@ else{
 $( "body" ).append('<div id="imagebig"><iframe id="loaderIframeIcon1" src="https://jimboy3100.github.io/extras/loader/loader.html" name="CodePen" allowfullscreen="true" sandbox="allow-scripts allow-pointer-lock allow-same-origin allow-popups allow-modals allow-forms" allowtransparency="true" scrolling="no" frameBorder="0" class="result-iframe" style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe></div>');
 //$( "body" ).append('<div id="imagebig"><iframe id="loaderIframeIcon1" src="https://jimboy3100.github.io/extras/banneranimated2EXPRESS.html" name="CodePen" allowfullscreen="true" sandbox="allow-scripts allow-pointer-lock allow-same-origin allow-popups allow-modals allow-forms" allowtransparency="true" scrolling="no" frameBorder="0" class="result-iframe" style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe></div>');
 }
-*/
+
 //load old skins
 
 (function(t, c, m) {
@@ -118,7 +118,7 @@ $( "body" ).append('<div id="imagebig"><iframe id="loaderIframeIcon1" src="https
       
    });
 })('tcm_old_store_skins', window, document);
-
+*/
 
 
 //Not yet loaded function
