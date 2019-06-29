76103b30ffa7ae7c80f2c9b515fa5c3edb9109ae
Legend Mod <jimboy3100@hotmail.com>
2019-06-29 23:37:46 +0300

    Update olddeals.js

--- LMexpress/olddeals.js
+++ LMexpress/olddeals.js
@@ -1,5 +1,5 @@
  //OLD DEALS
- //v2.1
+ //v2.2
  //for agarioUID, agarioID, look at the case 102: on this file https://jimboy3100.github.io/ogario/ogario.v4.js?v=32
 
  /* you will need this
@@ -23,11 +23,10 @@ else{
 */
  window.MiniclipConfigDestination = "https://configs-web.agario.miniclippt.com/live/v12/2168/GameConfiguration.json";
  window.MiniclipDestination = "https://configs-web.agario.miniclippt.com/live/v12/2168/";
- if (window.agarversion!=null){
-	 
- window.MiniclipConfigDestination = "https://configs-web.agario.miniclippt.com/live/" + window.agarversion + "GameConfiguration.json";
- window.MiniclipDestination = "https://configs-web.agario.miniclippt.com/live/" + window.agarversion;
- }
+ //if (window.agarversion!=null){	 
+ //window.MiniclipConfigDestination = "https://configs-web.agario.miniclippt.com/live/" + window.agarversion + "GameConfiguration.json";
+ //window.MiniclipDestination = "https://configs-web.agario.miniclippt.com/live/" + window.agarversion;
+// }
  SpecialDeals();
 
 
