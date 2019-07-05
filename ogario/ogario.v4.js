8d743decdc27b5fd51da015bb90e3ed42efc9b64
Legend Mod <jimboy3100@hotmail.com>
2019-07-05 13:26:08 +0300

    Update ogario.v4.js

--- ogario/ogario.v4.js
+++ ogario/ogario.v4.js
@@ -1,7 +1,7 @@
 // Open Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia
 // This is part of the Legend mod project
-// v1.961 MEGA TEST
+// v1.967 MEGA TEST
 // Game Configurations
 
 //window.testobjects = {};
@@ -6091,7 +6091,7 @@ var thelegendmodproject = function(t, e, i) {
                 for (var s = 0; s < t.length; s++) i.setUint8(s + 1, t.charCodeAt(s));
                 this.sendMessage(i);
             },
-            'sendPosition': function(cell, target2) {
+            'sendPosition': function(cell, target3) {
                 if (this.isSocketOpen() && this.connectionOpened && this.clientKey) {
                     if (!window.autoPlay) {
                         var t = window.legendmod.vector[window.legendmod.vnr][0] ? this.translateX(this.cursorX) : this.cursorX; //Sonia3
@@ -6104,16 +6104,22 @@ var thelegendmodproject = function(t, e, i) {
                     //autoplay handling
                     else {
                         //if (typeof cell != "undefined") { //when used, autoplay not working as expected
-                        if (Object.keys(target2).length == 0) {
-                            var t = window.legendmod.vector[window.legendmod.vnr][0] ? this.translateX(cell.x) : cell.x; //Sonia3
-                            var e = window.legendmod.vector[window.legendmod.vnr][1] ? this.translateY(cell.y) : cell.y; //Sonia3							
+						var target3;
+						if (target3==null){
+							target3={}
+						}						
+							if (Object.getOwnPropertyNames(target3).length == 0) {
+							var cell2={};
+							if (cell){cell2=cell};
+                            var t = window.legendmod.vector[window.legendmod.vnr][0] ? this.translateX(cell2.x) : cell2.x; //Sonia3
+                            var e = window.legendmod.vector[window.legendmod.vnr][1] ? this.translateY(cell2.y) : cell2.y; //Sonia3							
                             // var t = cell.x;
                             //var e = cell.y;
                         } else {
-                            var t = window.legendmod.vector[window.legendmod.vnr][0] ? this.translateX(target2.x) : target2.x; //Sonia3
-                            var e = window.legendmod.vector[window.legendmod.vnr][1] ? this.translateY(target2.y) : target2.y; //Sonia3								
-                            //var t = target2.x;
-                            //var e = target2.y;
+                            var t = window.legendmod.vector[window.legendmod.vnr][0] ? this.translateX(target3.x) : target3.x; //Sonia3
+                            var e = window.legendmod.vector[window.legendmod.vnr][1] ? this.translateY(target3.y) : target3.y; //Sonia3								
+                            //var t = target3.x;
+                            //var e = target3.y;
                         }
                         //}
                     }
