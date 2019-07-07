e5ec5d1cfe58a119a1768c587a2686402319d8a8
Legend Mod <jimboy3100@hotmail.com>
2019-07-07 07:44:44 +0300

    Update LanguagePackHandler.js

--- LanguagePackHandler.js
+++ LanguagePackHandler.js
@@ -1,6 +1,6 @@
 //Language Pack Handler
 //Legend Mod By Jimboy3100
-//v1.2e
+//v1.2f
 
 //Legend Mod By Jimboy3100
 var Languagetimesused;
@@ -64,6 +64,7 @@ $(".quick-quests.ogicon-trophy").attr('data-original-title',Languageletter23);
 $(".quick-more-skins.ogicon-grin").attr('data-original-title',Languageletter24);
 $(".quick-yt.ogicon-youtube2").attr('data-original-title',Languageletter25);
 $("#LegGoogleForm").attr('data-original-title',Languageletter25a);
+$("#themesBtn").attr('data-original-title',Languageletter126);
 $("#ModInfoQuick").attr('data-original-title',Languageletter25b);
 
 $("#more-skins").text(Languageletter26);
@@ -88,9 +89,18 @@ $(".options-box.zoomGroup>h5").text(Languageletter37b);
 $(".options-box.zoomGroup>LABEL").replace('Auto zoom', Languageletter37a);
 $(".options-box.zoomGroup>DIV>DIV>SPAN:nth-child(1)").text(Languageletter38 + ": ");
 
+//$(".options-box.leaderboard>h5").text(Languageletter38a);
+//$(".options-box.leaderboard>LABEL").replace('Leaderboard Players', Languageletter38b);
+//$(".options-box.leaderboard>LABEL").replace('Team Players', Languageletter38c);
+
 $(".options-box.respGroup>h5").text(Languageletter39);
 $(".options-box.respGroup>LABEL").replace('Quick respawn', Languageletter41);
 $(".options-box.respGroup>LABEL").replace('Auto respawn', Languageletter40);
+//$(".options-box.respGroup>LABEL").replace('Spawn special effects', Languageletter41a);
+//$(".options-box.massGroup>LABEL").replace('Merge timer BETA off', Languageletter41b);
+//$(".options-box.massGroup>LABEL").replace('Suggested to be enabled for Lag reduce', Languageletter41c);
+//$(".options-box.massGroup>LABEL").replace('Virus shot sound', Languageletter41d);
+
 $(".options-box.namesGroup>h5").text(Languageletter42);
 $(".options-box.namesGroup>LABEL").replace('No names', Languageletter43);
 $(".options-box.namesGroup>LABEL").replace('Optimized names', Languageletter44);
@@ -111,11 +121,18 @@ $(".options-box.skinsGroup>h5").text(Languageletter58);
 $(".options-box.skinsGroup>LABEL").replace('No skins', Languageletter59);
 $(".options-box.skinsGroup>LABEL").replace('Custom skins', Languageletter60);
 $(".options-box.skinsGroup>LABEL").replace('Vanilla skins', Languageletter61);
+//$(".options-box.skinsGroup>LABEL").replace('Jelly physics', Languageletter61a);
+//$(".options-box.skinsGroup>LABEL").replace('Video skins', Languageletter61b);
+//$(".options-box.skinsGroup>LABEL").replace('Sound from other\'s Video skins when both C3', Languageletter61c);
+
 $(".options-box.foodGroup>h5").text(Languageletter6a2);
 $(".options-box.foodGroup>LABEL").replace('Optimized food', Languageletter62);
 $(".options-box.foodGroup>LABEL").replace('Auto hide food', Languageletter63);
 $(".options-box.foodGroup>LABEL").replace('Auto hide food', Languageletter64);
 $(".options-box.foodGroup>LABEL").replace('Rainbow food', Languageletter65);
+//$(".options-box.foodGroup>LABEL").replace('mass', Languageletter49);
+//$(".options-box.foodGroup>LABEL").replace('zoom', Languageletter37b);
+
 $(".options-box.transparencyGroup>h5").text(Languageletter66);
 $(".options-box.transparencyGroup>LABEL").replace('No colors', Languageletter67);
 $(".options-box.transparencyGroup>LABEL").replace('My custom color', Languageletter68);
@@ -123,10 +140,15 @@ $(".options-box.transparencyGroup>LABEL").replace('My transparent skin', Languag
 $(".options-box.transparencyGroup>LABEL").replace('Transparent skins', Languageletter70);
 $(".options-box.transparencyGroup>LABEL").replace('Transparent cells', Languageletter70a);
 $(".options-box.transparencyGroup>LABEL").replace('Transparent viruses', Languageletter71);
+//$(".options-box.transparencyGroup>LABEL").replace('Virus Glow', Languageletter71a);
+//$(".options-box.transparencyGroup>LABEL").replace('Animated rainbow colors', Languageletter71b);
+
 $(".options-box.gridGroup>h5").text(Languageletter72);
 $(".options-box.gridGroup>LABEL").replace('Show grid', Languageletter73);
 $(".options-box.gridGroup>LABEL").replace('Show background sectors', Languageletter74);
 $(".options-box.gridGroup>LABEL").replace('Show map borders', Languageletter75);
+//$(".options-box.gridGroup>LABEL").replace('Border Glow', Languageletter75a);
+
 $(".options-box.chatGroup>h5").text(Languageletter76);
 $(".options-box.chatGroup>LABEL").replace('Disable chat', Languageletter77);
 $(".options-box.chatGroup>LABEL").replace('Sound notifications', Languageletter78);
@@ -138,24 +160,33 @@ $(".options-box.miniMapGroup>h5").text(Languageletter82a);
 $(".options-box.miniMapGroup>LABEL").replace('Show minimap', Languageletter82b);
 $(".options-box.miniMapGroup>LABEL").replace('Show minimap grid', Languageletter82c);
 $(".options-box.miniMapGroup>LABEL").replace('Show minimap guides', Languageletter82d);
+//$(".options-box.miniMapGroup>LABEL").replace('Show extra minimap guides', Languageletter82f);
+//$(".options-box.miniMapGroup>LABEL").replace('Show ghost cells', Languageletter82g);
 $(".options-box.miniMapGroup>LABEL").replace('One-colored teammates', Languageletter82e);
 $(".options-box.helpersGroup>h5").text(Languageletter83);
 $(".options-box.helpersGroup>LABEL").replace('Opponents colors', Languageletter84);
 $(".options-box.helpersGroup>LABEL").replace('Opponents rings', Languageletter85);
 $(".options-box.helpersGroup>LABEL").replace('Viruses colors', Languageletter86);
 $(".options-box.helpersGroup>LABEL").replace('Split range', Languageletter87);
+//$(".options-box.helpersGroup>LABEL").replace('Quick double split range', Languageletter87a);
+//$(".options-box.helpersGroup>LABEL").replace('Slow double split range', Languageletter87b);
 $(".options-box.helpersGroup>LABEL").replace('Viruses range', Languageletter88);
 $(".options-box.helpersGroup>LABEL").replace('Cursor tracking', Languageletter89);
 $(".options-box.helpersGroup>LABEL").replace('Teammates indicators', Languageletter90);
+//$(".options-box.helpersGroup>LABEL").replace('Ghost cells (fps drop)', Languageletter90a);
+//$(".options-box.helpersGroup>LABEL").replace('Ghost cells info (confusing)', Languageletter90b);
 $(".options-box.mouseGroup>h5").text(Languageletter91);
 $(".options-box.mouseGroup>LABEL").replace('LMB - Mouse split', Languageletter92);
 $(".options-box.mouseGroup>LABEL").replace('RMB - Mouse feed', Languageletter93);
 $(".options-box.mouseGroup>LABEL").replace('Invert mouse buttons', Languageletter94);
 $(".options-box.hudGroup>h5").text(Languageletter94a);
-$(".options-box.hudGroup>LABEL").replace('Show team top 5', Languageletter95);
+$(".options-box.hudGroup>LABEL").replace('Show teamboard', Languageletter95);
 $(".options-box.hudGroup>LABEL").replace('Show targeting', Languageletter96);
+//$(".options-box.hudGroup>LABEL").replace('Show leaderboard mass', Languageletter96a);
 $(".options-box.hudGroup>LABEL").replace('Centered leaderboard', Languageletter97);
+//$(".options-box.hudGroup>LABEL").replace('\"Leaderboard\" header ', Languageletter97a);
 $(".options-box.hudGroup>LABEL").replace('Game stats at the top', Languageletter98);
+//$(".options-box.hudGroup>LABEL").replace('Skins on teamboard', Languageletter98a);
 $(".options-box.statsGroup>h5").text(Languageletter99);
 $(".options-box.statsGroup>LABEL").replace('Show game stats', Languageletter100);
 $(".options-box.statsGroup>LABEL").replace('Game stats: Mass', Languageletter101);
@@ -172,6 +203,7 @@ $(".options-box.extrasGroup>LABEL").replace('Block popups', Languageletter109);
 $(".options-box.voiceGroup>.menu-main-color").text(Languageletter109a);
 $(".options-box.voiceGroup>LABEL").replace('Voice-Prefix', Languageletter109b);
 $(".options-box.voiceGroup>LABEL").replace('Voice-lang', Languageletter109c);
+//$(".options-box.voiceGroup>LABEL").replace('UnPause', Languageletter109d);
 
 $('#quality>option:nth-child(1)').text(Languageletter110);
 $('#quality>option:nth-child(2)').text(Languageletter111);
@@ -515,7 +547,8 @@ $("#potions").text(Languageletter349);
 $("#SpecialDealsBtn").text(Languageletter351);
 $("#SpecialDealsQuickBtn").attr("data-original-title", Languageletter351);
 $(".btn.btn-primary.btn-success.btn-leagues").text(Languageletter352); //Leaderboards
-$("#LegGoogleForm").attr("data-original-title", Languageletter353);
+
+//$("#LegGoogleForm").attr("data-original-title", Languageletter353);
 $("#VideoSkinsQuick").attr("data-original-title", Languageletter354);
 $("#MiniScripts").attr("data-original-title", Languageletter355);
 $("#OpenuserScripts").text(Languageletter356);
