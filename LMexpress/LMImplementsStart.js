f4ced689ce3705e3baf03421c9852a6d1aeba5f9
Legend Mod <jimboy3100@hotmail.com>
2019-09-13 16:51:30 +0300

    Update LMImplementsStart.js

--- LMexpress/LMImplementsStart.js
+++ LMexpress/LMImplementsStart.js
@@ -1,7 +1,7 @@
 //Legend Mod Sniff Mode by jimboy3100
 
 
-$( "body" ).append('<div id="imagebig"><iframe id="loaderIframeIcon1" src="https://jimboy3100.github.io/extras/banneranimatedImplementsLM.html" name="CodePen" allowfullscreen="true" sandbox="allow-scripts allow-pointer-lock allow-same-origin allow-popups allow-modals allow-forms" allowtransparency="true" scrolling="no" frameBorder="0" class="result-iframe" style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe></div>');
+$( "body" ).append('<div id="imagebig"><iframe id="loaderIframeIcon1" src="https://legendmod.ml/extras/banneranimatedImplementsLM.html" name="CodePen" allowfullscreen="true" sandbox="allow-scripts allow-pointer-lock allow-same-origin allow-popups allow-modals allow-forms" allowtransparency="true" scrolling="no" frameBorder="0" class="result-iframe" style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe></div>');
 
 
 //Not yet loaded function
