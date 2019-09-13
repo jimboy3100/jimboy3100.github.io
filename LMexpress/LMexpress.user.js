5eaef016aaf22e477e84f4bea8bfaa7cbdf75095
Legend Mod <jimboy3100@hotmail.com>
2019-09-13 18:39:12 +0300

    Update LMexpress.user.js

--- LMexpress/LMexpress.user.js
+++ LMexpress/LMexpress.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Legend Express Agar.io Extension
 // @namespace    Legend Express Agario Mod
-// @version      1.6
+// @version      1.7
 // @description  Agario Mod - Legend,Ogario,Kitty,Old Skins,Animated Skins,Language Packs,Manual User Scripts,Chat,60++ Macros/Hotkeys(Tricksplit,Doublesplit,Quick Feeding,Popsplit,Auto Coins,Freeze Cell Macro,Auto respawn)
 // @homepage     http://www.legendmod.ml
 // @author       Jimboy3100
