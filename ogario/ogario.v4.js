0b73929b7e08e75e542bcd2d29d81911bb7da986
Legend Mod <jimboy3100@hotmail.com>
2019-11-02 21:49:49 +0200

    Update ogario.v4.js

--- ogario/ogario.v4.js
+++ ogario/ogario.v4.js
@@ -1,7 +1,7 @@
 // Open Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia
 // This is part of the Legend mod project
-// v1.1530 MEGA TEST
+// v1.1531 MEGA TEST
 // Game Configurations
 
 //window.testobjects = {};
