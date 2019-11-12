e3555a6053d348160de4ccf7b612781144b1dd12
Legend Mod <jimboy3100@hotmail.com>
2019-11-12 02:45:10 +0200

    Update buffers.js

--- ExampleScripts/agario-bots2/buffers.js
+++ ExampleScripts/agario-bots2/buffers.js
@@ -13,7 +13,16 @@ module.exports = {
         writer.writeUint32(version)
         return writer.buffer
     },
-    spawn(name){
+    spawn(name,token){
+		//console.log(name)
+        let buf = new Buffer.alloc(3 + name.length + token.length);
+        buf.writeUInt8(0, 0);
+		buf.write(name, 1);
+        //buf.write(name, 1);
+        buf.write(token, 2 + name.length);
+        return buf		
+    },
+    spawn2(name){
         const writer = new Writer(2 + name.length)
         writer.writeUint8(0)
         writer.writeString(name)
