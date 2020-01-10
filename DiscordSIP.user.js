34a8135fd06f9badfbc7ce04e1b617f3442f5ad2
Legend Mod <jimboy3100@hotmail.com>
2020-01-10 11:01:04 +0200

    Update DiscordSIP.user.js

--- DiscordSIP.user.js
+++ DiscordSIP.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Send Agario SIP To Discord
 // @namespace    http://tampermonkey.net/
-// @version      1.9 test
+// @version      1.11 test
 // @description  Gets the agario server IP link and posts it to discord
 // @author       σмg ι ℓσνє уσυ! Published by Jimboy3100
 // @match        http://agar.io/*
@@ -120,8 +120,8 @@ var discordskintosend;
 if ( $( "#skin" ).length ) {
     discordskintosend = $( "#skin" ).val()
 }
-var discordtagtosend;
-if ( $( "#skin" ).length ) {
+var discordtagtosend = "No Tag";
+if ( $( "#discordtagtosend" ).length ) {
     discordtagtosend = $( "#clantag" ).val()
 }
 var discordregiontosend;
