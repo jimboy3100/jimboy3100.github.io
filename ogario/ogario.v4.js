91360a968553a7dc9e903742a671aeb89ca11f53
Legend Mod <jimboy3100@hotmail.com>
2020-03-11 18:36:25 +0200

    Update ogario.v4.js

--- ogario/ogario.v4.js
+++ ogario/ogario.v4.js
@@ -6718,7 +6718,7 @@ var thelegendmodproject = function(t, e, i) {
                             var point = this.points[i];
                             style.lineTo(point.x, point.y);
                         }
-                    } else if (defaultmapsettings.jellyPhisycs && this.isVirus && !window.testjellyPhisycsNoVirus) {
+                    } else if (defaultmapsettings.jellyPhisycs && this.isVirus) {
                         style.lineJoin = "miter"
                         var pointCount = 120;
                         var incremental = this.pi2 / pointCount;
