d02c975189538db872e994731ad3632df1497d15
Legend Mod <jimboy3100@hotmail.com>
2020-03-30 18:22:08 +0300

    Update libLM.user.js

--- ExampleScripts/libLM.user.js
+++ ExampleScripts/libLM.user.js
@@ -4,7 +4,7 @@
 // @version      0.4
 // @description  try to take over the world!
 // @author       You
-// @match        https://legendmod.ml/ogario/
+// @match        https://www.atmarkit.co.jp/d9KcsYjB
 // @grant        unsafeWindow
 // ==/UserScript==
 
