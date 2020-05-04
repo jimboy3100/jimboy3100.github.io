ef21b384c3140d973d13707e6127a3f00040755b
Legend Mod <jimboy3100@hotmail.com>
2020-05-04 20:02:29 +0300

    Update ogario.v4.js

--- ogario/ogario.v4.js
+++ ogario/ogario.v4.js
@@ -1,7 +1,7 @@
 // Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia
 // This is part of the Legend mod project
-// v1.531
+// v1.532
 
 
 //window.testobjects = {};
@@ -4226,7 +4226,7 @@ function thelegendmodproject() {
                 $(".btn-spectate, .btn-logout").appendTo("#agario-main-buttons");
                 $("#agario-main-buttons").addClass("clearfix").before('<div id="server-info" class="form-group clearfix"><input id="server-ws" class="form-control" placeholder="Server WS" style="display: none;">'+
 				'<button id="server-reconnect" class="btn btn-primary ogicon-redo2" style="display: none;"></button><input id="server-token" class="form-control" placeholder="Server token"><button id="server-join" class="btn btn-success" data-itr="page_join_party">Join</button></div>');
-				$(".btn.btn-warning.btn-server-info.ogicon-cogs").after('<button id="server-reconparty-tokenparty-tokennect" class="btn btn-success" style="display: inline-block; float: left; width: 11%; text-transform: capitalize;"><i class="fa fa-refresh"></i></button>');
+				$(".btn.btn-warning.btn-server-info.ogicon-cogs").after('<button id="server-reconnect" class="btn btn-success" style="display: inline-block; float: left; width: 11%; text-transform: capitalize;"><i class="fa fa-refresh"></i></button>');
 				$("#server-join").after('<div id="ogario-party" class="clearfix"><input id="party-token" class="form-control" placeholder="Party token"></div>');
 				$(".clearfix").show();
                 $("#ogario-party").append('<button id="join-party-btn-2" class="btn btn-success" data-itr="page_join_party">Join</button><button id="create-party-btn-2" class="btn btn-primary" data-itr="page_create_party" style="display: none;">Create</button>');
