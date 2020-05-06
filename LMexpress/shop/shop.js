189e519f20b8a9590a07cd02ecac7c5176c5d205
Legend Mod <jimboy3100@hotmail.com>
2020-05-06 14:53:44 +0300

    Update shop.js

--- LMexpress/shop/shop.js
+++ LMexpress/shop/shop.js
@@ -1,7 +1,7 @@
             $('#helloContainer').after('<div class="modal fade in" id="LMShop" aria-hidden="false" style="display: block;">' +
 				'<link rel="stylesheet" type="text/css" href="https://legendmod.ml/css/specialeffects.css">' +
 				'<div class="modal-backdrop fade in"></div>' +
-                '<div class="modal-dialog" style="top: calc(50vh - 241.5px); width: 820px; height:500">' +
+                '<div class="modal-dialog" style="top: calc(50vh - 241.5px); width: 780px; height:500">' +
                 '<div class="modal-content">' +
                 '<div id="CloseLMShop2" class="modal-header"><button id="CloseLMShop" type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">' + Premadeletter113 + '</span></button> <button id="FAQLMShop" type="button" class="close" data-dismiss="modal"><span aria-hidden="true">?</span><span class="sr-only">' + Premadeletter113 + '</span></button>' +
                 '<h4 class="modal-title" style="font-family: Roboto Condensed, sans-serif">' + 'Legend mod Shop' + '</h4>' +
@@ -14,30 +14,30 @@
         '<div id="nav">' +
             '<ul>' +
 				
-                '<li><a href="#imgur" class="active">Imgur</a></li>' +
-				'<li><a href="#lowres">Low Res-More FPS</a></li>' +
-				'<li><a href="#vanillaskins">Vanilla</a></li>' +			  		
-                '<li><input type="text" id="skin-url" placeholder="Skin URL" readonly></li>' +
+                '<li><a href="#imgur" class="active">Premium</a></li>' +
+				'<li><a href="#lowres">Free</a></li>' +
+				'<li><a href="#vanillaskins">Owned</a></li>' +			  		
+                '<li><input type="text" id="skin-url" placeholder="Special Skin Effect" readonly></li>' +
 		'<li><a id="UseEffect" class="link">Use</a></li>' +
 		    '</ul>' +
         '</div>' +
     '</div>' +
 
     '<div id="imgur" class="skins-wrapper">' +
-		'<div class="skin-box"><img class="lazy" data-original="https://i.imgur.com/kdma64U.png"></div>' +
-        '<div class="skin-box"><img class="lazy" data-original="https://i.imgur.com/UUDXxK2.jpg"></div>' +
-        '<div class="skin-box"><img class="lazy" data-original="https://i.imgur.com/z7aRxuH.png"></div>' +
-		'<div class="skin-box"><img class="lazy" data-original="https://i.imgur.com/SaiTVGU.png"></div>' +
-		'<div class="skin-box"><img class="lazy" data-original="https://i.imgur.com/zs2dxC4.png"></div>' +
-        '<div class="skin-box"><img class="lazy" data-original="https://i.imgur.com/stb0xll.jpg"></div>' +
-        '<div class="skin-box"><img class="lazy" data-original="https://i.imgur.com/mHxchM9.png"></div>' +
-        '<div class="skin-box"><img class="lazy" data-original="https://i.imgur.com/hJSyWRi.png"></div>' +
-		'<div class="skin-box"><img class="lazy" data-original="https://i.imgur.com/mOlOOBi.jpg"></div>' +
+		'<div class="skin-box"><img class="lazy" name="Hat" data-original="https://legendmod.ml/banners/iconSpecialSkinEffectsHat3.png"></div>' +
+        '<div class="skin-box"><img class="lazy" name="JellyFish" data-original="https://legendmod.ml/banners/iconSpecialSkinEffectsJellyFish.png"></div>' +
+        '<div class="skin-box"><img class="lazy" name="King" data-original="https://legendmod.ml/banners/iconSpecialSkinEffectsCrown.png"></div>' +
+		'<div class="skin-box"><img class="lazy" name="Smoke" data-original="https://legendmod.ml/banners/iconSpecialSkinEffectsSmoke.png"></div>' +
+		'<div class="skin-box"><img class="lazy" name="USA" data-original="https://legendmod.ml/banners/iconSpecialSkinEffectsUSA.png"></div>' +
+        '<div class="skin-box"><img class="lazy" name="Light" data-original="https://legendmod.ml/banners/iconSpecialSkinEffectsLight.png"></div>' +
+        '<div class="skin-box"><img class="lazy" name="Sword" data-original="https://legendmod.ml/banners/iconSpecialSkinEffectsSword.png"></div>' +
+        '<div class="skin-box"><img class="lazy" name="Mask" data-original="https://legendmod.ml/banners/iconSpecialSkinEffectsMask.png"></div>' +
+		'<div class="skin-box"><img class="lazy" name="Heart" data-original="https://legendmod.ml/banners/iconSpecialSkinEffectsHeart.png"></div>' +
+	    '<div class="skin-box"><img class="lazy" name="Vip" data-original="https://legendmod.ml/banners/iconSpecialSkinEffectsVip.png"></div>' +
+	    '<div class="skin-box"><img class="lazy" name="Youtube" data-original="https://legendmod.ml/banners/iconSpecialSkinEffectsYoutube.png"></div>' +		
 
    '</div>' +
 	        '<div id="lowres" class="skins-wrapper">' +
-	        '<div class="skin-box"><img class="lazy" data-original="https://legendmod.ml/lowresskins/1up-custom-skin.png"></div>' +
-	        '<div class="skin-box"><img class="lazy" data-original="https://legendmod.ml/lowresskins/ace-custom-skin.png"></div>' +
 	        '<div class="skin-box"><img class="lazy" data-original="https://legendmod.ml/lowresskins/adamzonetopmarks-custom-skin.png"></div>' +
 	        '<div class="skin-box"><img class="lazy" data-original="https://legendmod.ml/lowresskins/agariomods.com-custom-skin.png"></div>' +
 	        '<div class="skin-box"><img class="lazy" data-original="https://legendmod.ml/lowresskins/agar-youtube-custom-skin.png"></div>' +
@@ -58,7 +58,7 @@
 	'</div>' +
 	'</div>' +
     '<script src="https://legendmod.ml/skins/jquery.lazyload.min.js"></script>' +
-    '<script src="https://legendmod.ml/LMexpress/shop/shopscript.js"></script>' +				
+    '<script src="https://legendmod.ml/skins/skins.js"></script>' +				
                 '</div>' +
                 '</div>' +
                 '</div>' +
