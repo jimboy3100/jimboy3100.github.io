d8b10b2ab5329bb8c09d75fea1a478eeec24fa86
Legend Mod <jimboy3100@hotmail.com>
2020-05-06 15:08:11 +0300

    Update shop.js

--- LMexpress/shop/shop.js
+++ LMexpress/shop/shop.js
@@ -1,62 +1,72 @@
-//v0.4
-$(document).ready(function() {
-    $("#imgur img.lazy").lazyload({
-        effect : "fadeIn",
-        skip_invisible : true
-    });
-    
-    $("#hizliresim img.lazy").lazyload({
-        effect : "fadeIn",
-        skip_invisible : true
-    });
-     $("#lowres img.lazy").lazyload({
-        effect : "fadeIn",
-        skip_invisible : true
-    });	
-     $("#vanillaskins img.lazy").lazyload({
-        effect : "fadeIn",
-        skip_invisible : true
-    });
-	
-    $("img.lazy").on("click", function(e) {
-        e.preventDefault();
-		var url = $(this).attr("name");
-		
-        //var url = $(this).attr("src");
-        $("#skin-url").val(url).select();
-        try {
-            //document.execCommand("copy");
-        } catch (e) {}
-    });
-    
-    $("#skin-url").on("click", function(e) {
-        $(this).select();
-        try {
-            //document.execCommand("copy");
-        } catch (e) {}
-    });
+            $('#helloContainer').after('<div class="modal fade in" id="LMShop" aria-hidden="false" style="display: block;">' +
+				'<link rel="stylesheet" type="text/css" href="https://legendmod.ml/css/specialeffects.css">' +
+				'<div class="modal-backdrop fade in"></div>' +
+                '<div class="modal-dialog" style="top: calc(50vh - 241.5px); width: 780px; height:500">' +
+                '<div class="modal-content">' +
+                '<div id="CloseLMShop2" class="modal-header"><button id="CloseLMShop" type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">' + Premadeletter113 + '</span></button> <button id="FAQLMShop" type="button" class="close" data-dismiss="modal"><span aria-hidden="true">?</span><span class="sr-only">' + Premadeletter113 + '</span></button>' +
+                '<h4 class="modal-title" style="font-family: Roboto Condensed, sans-serif">' + 'Legend mod Shop' + '</h4>' +
+                '</div>' +
+				
+				'<div id="LMShop3">' +
+				
+        '<div id="customskins">' +
+    '<div id="navbar">' +
+        '<div id="nav">' +
+            '<ul>' +
+				
+                '<li><a href="#imgur" class="active">Premium</a></li>' +
+				'<li><a href="#lowres">Free</a></li>' +
+				'<li><a href="#vanillaskins">Owned</a></li>' +			  		
+                '<li><input type="text" id="skin-url" placeholder="Special Skin Effect" readonly></li>' +
+		'<li><a id="UseEffect" class="link">Use</a></li>' +
+		    '</ul>' +
+        '</div>' +
+    '</div>' +
 
-	$("#nav a").on("click", function() {
-        var _this = $(this);
-        if (_this.hasClass("link")) {
-            return;
-        }
-        $("#nav a").removeClass("active");
-        _this.addClass("active");
-        var id = _this.attr("href");
-        $(".skins-wrapper").hide();
-        $(id).show();
-        $(window).scrollTop(0).trigger("scroll");
-        return false;
-    });
-    $("#UseEffect").on("click", function(e) {
-        try {	
-        //    window.parent.postMessage("CustomSkins&?skin="+$("#skin-url").val(), "*"); 
-		toastr.info("<b>[SERVER]:</b> Special Effect activated");
-		window.application.sendSocket3Info("spfc", $("#skin-url").val())
-        } catch (e) {}
-    });
-	setTimeout(function() {
-		$("#nav a")[0].click();
-	}, 100);
-});
+    '<div id="imgur" class="skins-wrapper">' +
+		'<div class="skin-box"><img class="lazy" name="Hat" data-original="https://legendmod.ml/banners/iconSpecialSkinEffectsHat3.png"></div>' +
+        '<div class="skin-box"><img class="lazy" name="JellyFish" data-original="https://legendmod.ml/banners/iconSpecialSkinEffectsJellyFish.png"></div>' +
+        '<div class="skin-box"><img class="lazy" name="King" data-original="https://legendmod.ml/banners/iconSpecialSkinEffectsCrown.png"></div>' +
+		'<div class="skin-box"><img class="lazy" name="Smoke" data-original="https://legendmod.ml/banners/iconSpecialSkinEffectsSmoke.png"></div>' +
+		'<div class="skin-box"><img class="lazy" name="USA" data-original="https://legendmod.ml/banners/iconSpecialSkinEffectsUSA.png"></div>' +
+        '<div class="skin-box"><img class="lazy" name="Light" data-original="https://legendmod.ml/banners/iconSpecialSkinEffectsLight.png"></div>' +
+        '<div class="skin-box"><img class="lazy" name="Sword" data-original="https://legendmod.ml/banners/iconSpecialSkinEffectsSword.png"></div>' +
+        '<div class="skin-box"><img class="lazy" name="Mask" data-original="https://legendmod.ml/banners/iconSpecialSkinEffectsMask.png"></div>' +
+		'<div class="skin-box"><img class="lazy" name="Heart" data-original="https://legendmod.ml/banners/iconSpecialSkinEffectsHeart.png"></div>' +
+	    '<div class="skin-box"><img class="lazy" name="Vip" data-original="https://legendmod.ml/banners/iconSpecialSkinEffectsVip.png"></div>' +
+	    '<div class="skin-box"><img class="lazy" name="Youtube" data-original="https://legendmod.ml/banners/iconSpecialSkinEffectsYoutube.png"></div>' +		
+
+   '</div>' +
+	        '<div id="lowres" class="skins-wrapper">' +
+	        '<div class="skin-box"><img class="lazy" data-original="https://legendmod.ml/lowresskins/adamzonetopmarks-custom-skin.png"></div>' +
+	        '<div class="skin-box"><img class="lazy" data-original="https://legendmod.ml/lowresskins/agariomods.com-custom-skin.png"></div>' +
+	        '<div class="skin-box"><img class="lazy" data-original="https://legendmod.ml/lowresskins/agar-youtube-custom-skin.png"></div>' +
+	        '<div class="skin-box"><img class="lazy" data-original="https://legendmod.ml/lowresskins/alaska-custom-skin.png"></div>' +
+	        '<div class="skin-box"><img class="lazy" data-original="https://legendmod.ml/lowresskins/albania-custom-skin.png"></div>' +
+	        '<div class="skin-box"><img class="lazy" data-original="https://legendmod.ml/lowresskins/alexelcapo-custom-skin.png"></div>' +
+	        '<div class="skin-box"><img class="lazy" data-original="https://legendmod.ml/lowresskins/android-custom-skin.png"></div>' +
+	        '<div class="skin-box"><img class="lazy" data-original="https://legendmod.ml/lowresskins/anonymous-custom-skin.png"></div>' +
+
+	'</div>' +
+        '<div id="vanillaskins" class="skins-wrapper">' +
+        '<div class="skin-box"><img class="lazy" data-original="https://legendmod.ml/vanillaskins/Alien2_Gamma.png"></div>' +
+        '<div class="skin-box"><img class="lazy" data-original="https://legendmod.ml/vanillaskins/Alien2_Neila.png"></div>' +
+        '<div class="skin-box"><img class="lazy" data-original="https://legendmod.ml/vanillaskins/Alien2_Omicron.png"></div>' +
+        '<div class="skin-box"><img class="lazy" data-original="https://legendmod.ml/vanillaskins/Alien2_Smyg.png"></div>' +
+        '<div class="skin-box"><img class="lazy" data-original="https://legendmod.ml/vanillaskins/Alien2_Vega.png"></div>' +
+
+	'</div>' +
+	'</div>' +
+    '<script src="https://legendmod.ml/skins/jquery.lazyload.min.js"></script>' +
+    '<script src="https://legendmod.ml/LMexpress/shop/shopscript.js"></script>' +				
+                '</div>' +
+                '</div>' +
+                '</div>' +
+                '</div>');
+
+            $("#CloseLMShop").click(function() {
+                $("#LMShop").remove();
+            });
+            $("#FAQLMShop").click(function() {
+				window.open('https://legendmod.ml/', '_blank');
+            });	
