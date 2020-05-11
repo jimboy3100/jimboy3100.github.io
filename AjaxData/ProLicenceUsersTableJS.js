dad3295f798f833aaa21611cbaaf6d37a10fe2c4
Legend Mod <jimboy3100@hotmail.com>
2020-05-11 21:30:28 +0300

    Update ProLicenceUsersTableJS.js

--- AjaxData/ProLicenceUsersTableJS.js
+++ AjaxData/ProLicenceUsersTableJS.js
@@ -17,9 +17,9 @@ ProLicenceUsersTable = {
             "reason": "Hendrik Schönfelder",
             "notes": "Youtuber"
         },
-        "f3057c2d-9cb0-49c2-af90-afd5eb652f6c": {
-            "reason": "ƘıŋǥҜƦƉ",
-            "notes": "FavourMayDonate"
+        "a3742f28-059a-4ea1-abf7-566e69ec97f4": {
+            "reason": "Moderator",
+            "notes": "Snez"
         },        
         "797c0547-40b3-4d8c-86ac-2b1082156bd6": {
             "reason": "BadSfah",
