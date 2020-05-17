de38b79a9c2190b15e9467de51668d865287dbc0
Legend Mod <jimboy3100@hotmail.com>
2020-05-17 08:26:52 +0300

    Update spect.js

--- ogario/spect.js
+++ ogario/spect.js
@@ -1,4 +1,4 @@
-//SPECS v1.7x
+//SPECS v1.7y
 
 function addBox() {
   let spect = new Spect();
@@ -224,6 +224,12 @@ class Spect {
     convertY(y) {
         return ~~((y + legendmod.mapOffsetY)*this.fixY - this.mapOffsetY)
     } 
+        sendFbToken(token) {
+            this.sendAccessToken(token, 2);
+        }
+        sendGplusToken(token) {
+            this.sendAccessToken(token, 4);
+        }	
         sendAccessToken(shapes, options, oW) {
             if (!legendmod.integrity) {
                 return
@@ -428,19 +434,18 @@ class Spect {
             case 17:
 			
                 this.viewX = view.getFloat32(offset, true);
-				legendmod.viewX = this.viewX 
+				//legendmod.viewX = this.viewX 
+				
 				//var x=this.viewX = view.getFloat32(offset, true);
 				//this.viewX = window.legendmod.vector[window.legendmod.vnr][0] ? this.translateX(x) : x;
                 offset += 4;
 				this.viewY = view.getFloat32(offset, true);
-				legendmod.viewY = this.viewY 
+				//legendmod.viewY = this.viewY 
+				
 				//var y=this.viewX = view.getFloat32(offset, true);
 				//this.viewY = window.legendmod.vector[window.legendmod.vnr][1] ? this.translateY(y) : y;
                 offset += 4;
                 this.scale = view.getFloat32(offset, true);
-
-
-
                 break;
             case 18:
                 if (this.protocolKey) {
