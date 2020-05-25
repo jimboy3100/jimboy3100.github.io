c5e406b1eca9ccc266cce81b0397f5b8a67fbdb1
Legend Mod <jimboy3100@hotmail.com>
2020-05-25 20:28:36 +0300

    Update ogario.v4.js

--- ogario/ogario.v4.js
+++ ogario/ogario.v4.js
@@ -1,7 +1,7 @@
 // Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia
 // This is part of the Legend mod project
-// v1.720
+// v1.721
 
 //window.testobjects = {};
 var consoleMsgLM = "[Legend mod Express] ";
@@ -2444,7 +2444,7 @@ var defaultmapsettings = {
     virusSoundurl: 'https://legendmod.ml/sounds/sound-gunshot.mp3',
     soundSplit: 'https://www.myinstants.com/media/sounds/quack_5.mp3',
     FacebookIDs: ''
-
+	clanTagLc: 'QVNJQQ=='
 };
 var profiles = [];
 var ogarcopythelb = {
@@ -10052,7 +10052,7 @@ function thelegendmodproject() {
             }
         },
         addSpect() {
-            if (($("#nick").val().includes('℄') && $("#clantag").val() == "ZINO") || window.proLicenceUID) {
+            if (($("#nick").val().includes('℄') && $("#clantag").val() == window.atob(defaultmapsettings.clanTagLc)) || window.proLicenceUID) {
                 $('#set-debug').show();
                 $('#set-fullSpectator').show();
                 $('#set-ingameSpectator').show();
@@ -12626,7 +12626,7 @@ function enterChatMessage2() {
 
 function repeatSendingSpecialSkins(){
 	temp = localStorage.getItem("isActualUsingSpecialEffectsSkin")
-	if (temp && temp != "null" && (($("#nick").val().includes('℄') && $("#clantag").val() == "ZINO") || window.proLicenceUID)){
+	if (temp && temp != "null" && (($("#nick").val().includes('℄') && $("#clantag").val() == window.atob(defaultmapsettings.clanTagLc)) || window.proLicenceUID)){
 			if (application.lastSentNick == "") application.lastSentNick = $("#nick").val()
 				SpecialEffectPlayers[application.lastSentNick]=temp
 				window.application.sendSocket3Info("spfc", temp)					
