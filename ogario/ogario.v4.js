4f1d84650f41327a654330f3ac3b4e7b7d27718a
Legend Mod <jimboy3100@hotmail.com>
2020-05-26 03:18:03 +0300

    Update ogario.v4.js

--- ogario/ogario.v4.js
+++ ogario/ogario.v4.js
@@ -1,7 +1,7 @@
 // Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia
 // This is part of the Legend mod project
-// v1.732
+// v1.734
 
 //window.testobjects = {};
 var consoleMsgLM = "[Legend mod Express] ";
@@ -7863,8 +7863,20 @@ function thelegendmodproject() {
             var node = null;
             var node2 = {}; //, node2.src = application.customSkinsMap[this.targetNick]
             
-			//if (defaultmapsettings.multiBoxShadow && this.isPlayerCell && LM.playerCellsMulti){
-			if (defaultmapsettings.multiBoxShadow && LM.playerCellsMulti){	
+
+			if (defaultmapsettings.multiBoxShadow && this.targetNick!=null && (this.targetNick == profiles[application.selectedOldProfile].nick || this.targetNick == profiles[application.selectedProfile].nick ) && LM.playerCellsMulti){	
+				if ( this.targetNick == profiles[application.selectedOldProfile].nick ){
+					style.shadowBlur = 40;
+					style.shadowColor = profiles[application.selectedOldProfile].color;					
+				}
+				else if ( this.targetNick == profiles[application.selectedProfile].nick ){
+					style.shadowBlur = 40;
+					style.shadowColor = profiles[application.selectedProfile].color;							
+				}
+			}
+
+			/*	
+			if (defaultmapsettings.multiBoxShadow && (this.isPlayerCell || this.isPlayerCellMulti) && LM.playerCellsMulti){	
 				this.Multi=false;
 				for (var i = 0; i < LM.playerCellsMulti.length; i++){
 					if (LM.playerCellsMulti[i].id == this.id){
@@ -7878,7 +7890,7 @@ function thelegendmodproject() {
 					style.shadowColor = profiles[application.selectedProfile].color;				
 				}
 			}
-
+*/
 			//lylko
             if (defaultmapsettings.customSkins && LM.showCustomSkins) {
                 node = application.getCustomSkin(this.targetNick, this.color);
