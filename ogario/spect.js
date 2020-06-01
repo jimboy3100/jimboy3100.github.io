d305a7bf19884703a77285c3b3ec612c91d79dc2
Legend Mod <jimboy3100@hotmail.com>
2020-06-01 07:09:09 +0300

    Update spect.js

--- ogario/spect.js
+++ ogario/spect.js
@@ -1,4 +1,4 @@
-//SPECS v3.6d WORKS UNTIL HERE
+//SPECS v3.6f WORKS UNTIL HERE
 
 function loadMultiCellSkin(){
 	
@@ -780,7 +780,7 @@ class Spect {
 			y - distance > legendmod.viewYTrue + y2s) //>legendmod.camMaxY
     }		
     isInViewCustom3 (x , y, size) {
-			var randomNum = -50 // randomNum=40
+			var randomNum = +50 // randomNum=40
 			var distance = size + randomNum
             return !(x + distance < legendmod.camMinMultiX ||
 			y + distance < legendmod.camMinMultiY ||
@@ -937,7 +937,8 @@ class Spect {
 			//if (this.player && isVirus && !isFood && !invisible){
 			if (this.player && (isVirus || isFood) && !remove){
 				//remove = this.isInViewCustom(x , y, size) 
-				remove = (this.isInViewCustom(x , y, size) && this.isInViewCustom3(x , y, size))
+				if (isFood) remove = this.isInViewCustom(x , y, size)
+				if (isVirus) remove = (this.isInViewCustom(x , y, size) && this.isInViewCustom3(x , y, size))
 				//if (!invisible) invisible = (this.isInViewCustom(x , y, size) && this.isInViewCustom3(x , y, size))				
 			}			
 			if (isFood && !defaultmapsettings.rainbowFood){
