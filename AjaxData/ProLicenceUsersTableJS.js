f582fbdce47de2681bb394b76458f5eb31109f60
Uokhbhvbhjhbj <65785604+Uokhbhvbhjhbj@users.noreply.github.com>
2020-06-13 12:05:54 +0200

    Update ProLicenceUsersTableJS.js

--- AjaxData/ProLicenceUsersTableJS.js
+++ AjaxData/ProLicenceUsersTableJS.js
@@ -140,6 +140,10 @@ ProLicenceUsersTable = {
         "a21f4130-3474-4331-add1-bd02155e607c": {
             "reason": "Donate",
             "Chosenbyddevilowo": "sondick"
+        },   
+        "8edf92ef-1fed-4022-948c-a1308cfa3adf": {
+            "reason": "Give@20200720",
+            "Chosenbyddevilowo": "haine"
         },				
         "4e50ef3d-626e-4bf6-a376-2a0fc0629864": {
             "reason": "Donate",
