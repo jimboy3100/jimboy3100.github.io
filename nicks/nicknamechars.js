171973af73978a037deb4e8c423e49694de40e77
Legend Mod <jimboy3100@hotmail.com>
2020-08-02 16:58:18 +0300

    Update nicknamechars.js

--- nicks/nicknamechars.js
+++ nicks/nicknamechars.js
@@ -2,9 +2,9 @@ $("#main-menu").hide();
 $("#skins-panel").hide();
 $("#quick-menu").hide();
 $("#exp-bar").hide();
-$("#menu-footer").after('<div id="legendchars" style="background-image: url('+legbgpic+'); background-color: '+legbgcolor+'; border: 1px solid black; position:absolute;  height: 560px; width: 350px; margin-top: 0px;margin-left: middle; ">'+
+$("#menu-footer").after('<div id="legendchars" style="background-image: url('+legbgpic+'); background-color: '+legbgcolor+'; border: 1px solid black; position:absolute;  height: 520px; width:510px; margin-top: 0px;margin-left: middle; ">'+
 	'<div id="Userscriptshud2" style="display:block; margin-left: 10px; margin-right: 10px;">'+ //margin-left: 10px"
-	'<br><div id="Userscriptshud3"><iframe id="customskinsIframe2" src="https://jimboy3100.github.io/nicks/nicknamechars.html" width="330" height="490" >'+
+	'<br><div id="Userscriptshud3"><iframe id="customskinsIframe2" src="https://jimboy3100.github.io/nicks/" width="490" height="490" >'+
     '<p>Your browser does not support iframes.</p>'+
 	'</iframe>'	+
 											'</div></div>'+
