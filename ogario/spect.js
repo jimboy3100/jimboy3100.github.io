fee9509e2bae88cd7dbce122ab9657c2dac98a06
Legend Mod <jimboy3100@hotmail.com>
2020-08-15 01:11:50 +0300

    Update spect.js

--- ogario/spect.js
+++ ogario/spect.js
@@ -1,4 +1,4 @@
-//SPECS v3.9q WORKS UNTIL HERE
+//SPECS v3.9r WORKS UNTIL HERE
 
 function loadMultiCellSkin(){
 	
@@ -710,13 +710,13 @@ class Spect {
 	}	
     getX(x) {
       if(this.ghostFixed && this.mapOffsetFixed) {
-        return ((x + this.mapOffsetX)*this.fixX - legendmod.mapOffsetX + this.fix3x)		
+        return ((x + this.mapOffsetX)*this.fixX - legendmod.mapOffsetX - this.fix3x)		
 		//return ~~((x + this.mapOffsetX)*this.fixX - legendmod.mapOffsetX)
       }
     }
     getY(y) {
       if(this.ghostFixed && this.mapOffsetFixed) {
-		return ((y + this.mapOffsetY)*this.fixY - legendmod.mapOffsetY + this.fix3y)
+		return ((y + this.mapOffsetY)*this.fixY - legendmod.mapOffsetY - this.fix3y)
         //return ~~((y + this.mapOffsetY)*this.fixY - legendmod.mapOffsetY)
       }
     }
