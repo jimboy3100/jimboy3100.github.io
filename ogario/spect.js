78c5b199b20ee3656576e2d6ffefeafa8e2c0399
Legend Mod <jimboy3100@hotmail.com>
2020-08-16 20:17:26 +0300

    Update spect.js

--- ogario/spect.js
+++ ogario/spect.js
@@ -1,4 +1,4 @@
-//SPECS v4.1s WORKS UNTIL HERE
+//SPECS v4.1t WORKS UNTIL HERE
 
 function loadMultiCellSkin(){
 	
@@ -749,11 +749,8 @@ class Spect {
 	}
 */	
 	constantrecalculation3(x,y){	
-		//this.fix3x = (legendmod.playerCells[0].x - x) //* this.fixX
-		//this.fix3y = (legendmod.playerCells[0].y - y) //* this.fixY
-		this.fix3x = (legendmod.playerCells[0].targetX - x) //* this.fixX
-		this.fix3Y = (legendmod.playerCells[0].targetY - y) //* this.fixX
-		toastr.warning(this.number +  " targetX: " + legendmod.playerCells[0].targetX + " x: " + x + " targetY: " +  legendmod.playerCells[0].targetY + " y: " + y);
+		this.fix3x = (legendmod.playerCells[0].x - x) //* this.fixX
+		this.fix3y = (legendmod.playerCells[0].y - y) //* this.fixY
 		toastr.warning(this.number +  " px: " + legendmod.playerCells[0].x + " x: " + x + " py: " +  legendmod.playerCells[0].y + " y: " + y);
 		toastr.warning(this.number +  " fixX: " + this.fixX + " fixY: " + this.fixY);
 		
