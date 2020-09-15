3d10ee6ea548e8ae694f73d514c38de3405267fa
Legend Mod <jimboy3100@hotmail.com>
2020-09-15 04:13:05 +0300

    Update spect.js

--- ogario/spect.js
+++ ogario/spect.js
@@ -1,4 +1,4 @@
-//SPECS v4.4o WORKS UNTIL HERE
+//SPECS v4.4p WORKS UNTIL HERE
 
 function loadMultiCellSkin(){
 	
@@ -1003,20 +1003,22 @@ class Spect {
 			
 			
         }
-		if (!legendmod.integrity && this.player){
-			this.handleSendNick()			
-		}
-		else if (!legendmod.integrity && !this.player){
-            this.sendSpectate();
-        }
-        if(this.staticX!=null&&this.staticY!=null) {
-            setInterval(() => {
-                this.sendPosition(this.convertX(this.staticX), this.convertY(this.staticY));
-            }, 50);
-			if (!this.player){
-				this.sendFreeSpectate()
+		if (!legendmod.integrity){
+			if (this.player){
+				this.handleSendNick()			
 			}
-        }		
+			else if (!this.player){
+				this.sendSpectate();
+			}
+			if(this.staticX!=null&&this.staticY!=null) {
+				setInterval(() => {
+					this.sendPosition(this.convertX(this.staticX), this.convertY(this.staticY));
+				}, 50);
+				if (!this.player){
+					this.sendFreeSpectate()
+				}
+			}
+		}		
     }	
 	
 	terminate(){
