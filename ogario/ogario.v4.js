9897e27ecfa32bc8598d18fd131840272634277a
Legend Mod <jimboy3100@hotmail.com>
2020-09-29 20:23:38 +0300

    Update ogario.v4.js

--- ogario/ogario.v4.js
+++ ogario/ogario.v4.js
@@ -1,7 +1,7 @@
 // Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 // This is part of the Legend mod project
-// v2.601 testing
+// v2.602 testing
 
 //window.testobjects = {};
 var consoleMsgLM = "[Client] ";
@@ -4455,7 +4455,10 @@ function thelegendmodproject() {
 					if(drawRender.averageRenderTime>70)  color = 'color:red'
 					else if(drawRender.averageRenderTime>50)  color = 'color:yellow'
 					else if(drawRender.averageRenderTime<50)  color = 'color:green'
-					t += '💻: <span style=' + color + '>' + drawRender.averageRenderTime + '%</span> | ';
+					t += '💻: <span style=' + color + '>' + drawRender.averageRenderTime + '%</span>'
+					if (defaultmapsettings.showStatsFPS) {
+						t += ' | '
+					}
 				}
                 if (defaultmapsettings.showStatsFPS) {
                     t += 'FPS: ' + drawRender.fps;
