805ed9c3ec090f9c32bc9eb01baadb58c53a54ab
Legend Mod <jimboy3100@hotmail.com>
2020-10-15 04:10:40 +0300

    Update core.js

--- ExampleScripts/agarvaii/js/core.js
+++ ExampleScripts/agarvaii/js/core.js
@@ -1,4 +1,4 @@
-//v0.2
+//v0.3
 function UI() {
     function enter() {
         return $("#nick").val(UI.getName()), nodeList[0][1] == UI.getName() ? false : (nodeList[0][1] = UI.getName(), setLocalStorage("nick", $("#nick").val()), player_profile[selected_profile].name = UI.getName(), data(), true);
@@ -2869,7 +2869,7 @@ var announcementSent = false;
             cb(buf);
             //buf.setUint8(0, 80);
             //var i = 0;
-            cb(buf);
+            //cb(buf);
             oncomplete();
         };
         ws.onmessage = onmessage;
@@ -2923,7 +2923,9 @@ var announcementSent = false;
         }
         clockseq++;
         var offset = 0;
-        switch (240 == view.getUint8(offset) && (offset += 5), view.getUint8(offset++)) {
+		var opcode = data.getUint8(offset++);
+        //switch (240 == view.getUint8(offset) && (offset += 5), view.getUint8(offset++)) {
+			switch (54 == opcode && (opcode = 53), opcode) {
             case 16:
                 fn(view, offset);
                 break;
