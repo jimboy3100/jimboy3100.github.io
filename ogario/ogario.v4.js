466283ec8dfbb09880f7c5c07de652e6b46d34b4
Legend Mod <jimboy3100@hotmail.com>
2020-11-16 01:39:23 +0200

    Update ogario.v4.js

--- ogario/ogario.v4.js
+++ ogario/ogario.v4.js
@@ -1,5 +1,5 @@
 /* Source script
-v2.852
+v2.853
 Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 This is part of the Legend mod project
 IF YOU A NORMAL PERSON AND CARE ABOUT YOUR HEALTH, DON'T READ THIS SCRIPT
@@ -11361,8 +11361,9 @@ window.MouseClicks=[];
                             window.agarioUID = localStorage.getItem("agarioUID");
                             window.agarioID = localStorage.getItem("agarioID");
                         }
-                        if (window.agarioUID && UIDcontroller) {
-                            UIDfunction();
+                        if (window.agarioUID && UIDcontroller && !window.checkOneTheUID) {
+                            window.checkOneTheUID = true;
+							UIDfunction();
                         }
                         if (window.testobjects2.split('"�')[1]) {
                             window.agarioEncodedUID = window.testobjects2.split('"�')[1].split('=')[0] + "%3D";
