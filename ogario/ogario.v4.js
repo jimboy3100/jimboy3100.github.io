7aa109d5a45d1b271b3e34b92ed1b35ed3e18911
Legend Mod <jimboy3100@hotmail.com>
2020-12-10 04:29:11 +0200

    Update ogario.v4.js

--- ogario/ogario.v4.js
+++ ogario/ogario.v4.js
@@ -1,5 +1,5 @@
 /* Source script
-v2.957a
+v2.961
 Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 This is part of the Legend mod project
 IF YOU A NORMAL PERSON AND CARE ABOUT YOUR HEALTH, DON'T READ THIS SCRIPT
@@ -14604,8 +14604,8 @@ Game name     : ${i.displayName}<br/>
 								this.preDrawPelletColors(food[length].color);
 							}
 							else{
-								if (!window.xx5) ctx.drawImage(this.pelletColored[food[length].color], x, y);
-								if (window.xx5) ctx.drawImage(this.pelletColored[food[length].color], x, y, 10 + defaultSettings.foodSize, 10 + defaultSettings.foodSize);
+								ctx.drawImage(this.pelletColored[food[length].color], x, y);
+								//ctx.drawImage(this.pelletColored[food[length].color], x, y, (10 + defaultSettings.foodSize)*2, (10 + defaultSettings.foodSize)*2);
 							}
 						}
 						
@@ -15076,12 +15076,13 @@ Game name     : ${i.displayName}<br/>
             this.pelletColored[color] = null;
             var size = 10 + defaultSettings.foodSize;
             var canvas = document.createElement('canvas');
-            //canvas.width = 2 * size,
-            //canvas.height = 2 * size;
-			canvas.width = size,
-            canvas.height = size;
+            canvas.width = 2 * size,
+            canvas.height = 2 * size;
+			//canvas.width = size,
+            //canvas.height = size;
             var ctx = canvas.getContext('2d');
-            ctx.arc(size, size, size, 0, this.pi2, false);
+            //ctx.arc(size/2, size/2, size/2, 0, this.pi2, false);
+			ctx.arc(size, size, size, 0, this.pi2, false);
             //ctx.fillStyle = defaultSettings.foodColor;
 			ctx.fillStyle = color;
             ctx.fill();
