1b2be0270279bebc64997f90b9090a62b33ee410
Legend Mod <jimboy3100@hotmail.com>
2020-12-17 05:04:31 +0200

    Update ogario.v4.js

--- ogario/ogario.v4.js
+++ ogario/ogario.v4.js
@@ -1,5 +1,5 @@
 /* Source script
-v2.961
+v2.968
 Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 This is part of the Legend mod project
 IF YOU A NORMAL PERSON AND CARE ABOUT YOUR HEALTH, DON'T READ THIS SCRIPT
@@ -14022,14 +14022,29 @@ Game name     : ${i.displayName}<br/>
 					if (!legendmod.gridPic){
 		            legendmod.gridPic = new Image;
                     legendmod.gridPic.src = "https://legendmod.ml/banners/grid3.png";
+					//legendmod.gridPic.src = "https://legendmod.ml/banners/grid5.png";
 					}
 					//this.ctx.drawImage(application.customSkinsCache["test_cached"],
-					this.ctx.drawImage(legendmod.gridPic,			
+					/*this.ctx.drawImage(legendmod.gridPic,			
                         legendmod.mapMinX,
                         legendmod.mapMinY,
                         legendmod.mapMaxX - legendmod.mapMinX,
                         legendmod.mapMaxY - legendmod.mapMinY
-                    );
+                    );*/
+					this.ctx.drawImage(legendmod.gridPic,			
+						(legendmod.viewMinX - legendmod.mapMinX) / (legendmod.mapMaxX - legendmod.mapMinX) * legendmod.gridPic.width, 
+						(legendmod.viewMinY - legendmod.mapMinY) / (legendmod.mapMaxY - legendmod.mapMinY) * legendmod.gridPic.height,
+						(legendmod.viewMaxX - legendmod.viewMinX) / (legendmod.mapMaxX - legendmod.mapMinX) * legendmod.gridPic.width,
+						(legendmod.viewMaxY - legendmod.viewMinY) / (legendmod.mapMaxY - legendmod.mapMinY) * legendmod.gridPic.height,                    
+						legendmod.viewMinX,
+						legendmod.viewMinY,
+						//legendmod.mapMinX,
+						//legendmod.mapMinY,		
+						legendmod.viewMaxX - legendmod.viewMinX,
+						legendmod.viewMaxY - legendmod.viewMinY
+                        //legendmod.mapMaxX - legendmod.mapMinX,
+                        //legendmod.mapMaxY - legendmod.mapMinY
+                    );					
 		},			
         drawCustomBackgrounds() {		
             if (defaultSettings.customBackground && defaultSettings.customBackground != "") {			
@@ -14587,6 +14602,7 @@ Game name     : ${i.displayName}<br/>
             if (!food.length) {
                 return;
             }
+			
             if (defaultmapsettings.optimizedFood && this.pellet) {
 
                 for (var length = 0; length < food.length; length++) {
