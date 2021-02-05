9ca95939743b36e5ff2aff20b256e1f5b7078b0e
Legend Mod <jimboy3100@hotmail.com>
2021-02-05 13:12:46 +0200

    Update ogario.v4.js

--- ogario/ogario.v4.js
+++ ogario/ogario.v4.js
@@ -1,5 +1,5 @@
 /* Source script
-v3.050
+v3.051
 Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 This is part of the Legend mod project
 IF YOU A NORMAL PERSON AND CARE ABOUT YOUR HEALTH, DON'T READ THIS SCRIPT
@@ -9811,7 +9811,7 @@ window.MouseClicks=[];
 					//this.drawCircle(style, this.x, this.y, y, this.color)
 					if (this.isVirus || defaultmapsettings.cellContours || defaultmapsettings.transparentCells || defaultmapsettings.transparentSkins || this.isPlayerCell || this.playerCellsMulti){ //this is the normal function
 						style.arc(this.x, this.y, y, 0, this.pi2, false);
-						style.closePath();	
+						//style.closePath();	
 					}	
 					else{
 						this.drawCircle(style, this.x, this.y, y, this.color)
