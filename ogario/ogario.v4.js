e8c6769ede2619b3a1681c17e3b91ae1355cbbcb
Legend Mod <jimboy3100@hotmail.com>
2023-04-12 07:41:44 +0300

    Update ogario.v4.js

--- ogario/ogario.v4.js
+++ ogario/ogario.v4.js
@@ -1,4 +1,4 @@
-window.OgVer=3.240;
+window.OgVer=3.241;
 /* Source script
 Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 This is part of the Legend mod project
