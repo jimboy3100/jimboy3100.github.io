2a3a61156d47f4d793cf60b2e74474d0f2afb0f2
Legend Mod <jimboy3100@hotmail.com>
2024-03-03 23:25:51 +0200

    Update oldextension.js

--- extension/oldextension.js
+++ extension/oldextension.js
@@ -132,7 +132,7 @@ if (location.host == "talky.io") {
     }, 2000);
     setTimeout(function() {
         document.getElementsByClassName('SDuUr')[1].value = gamename;
-        //<a href="http://legendmod.ml" target="_blank" id="LegendModWebsite" class="title" style=""><u>Legend Mod</u></a> <a href=IPAgario target="_blank" id="IPAgario" class="title" style=""><u>Copy Agar.io Token</u></a>
+        //<a href="http://jimboy3100.github.io" target="_blank" id="LegendModWebsite" class="title" style=""><u>Legend Mod</u></a> <a href=IPAgario target="_blank" id="IPAgario" class="title" style=""><u>Copy Agar.io Token</u></a>
 
 		if (IPAgario.length==6){
 		document.getElementsByClassName('_24sME message message-info message-full-width')[0].before("[Talky.io]: Legend Mod. Server: " + "http://agar.io/#" + IPAgario + " . [PARTY] (Password rooms are different than Public)");}
