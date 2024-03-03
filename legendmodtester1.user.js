f4d0b736aa9275887cf8ce3357f46eb1a0b3ee27
Legend Mod <jimboy3100@hotmail.com>
2024-03-03 21:59:37 +0200

    Update legendmodtester1.user.js

--- legendmodtester1.user.js
+++ legendmodtester1.user.js
@@ -3,7 +3,7 @@
 // @namespace    Legend Agario Mod
 // @version      3.1
 // @description  Agario Mod - Legend,Ogario,Kitty,Old Skins,Animated Skins,Language Packs,Manual User Scripts,Chat,60++ Macros/Hotkeys(Tricksplit,Doublesplit,Quick Feeding,Popsplit,Auto Coins,Freeze Cell Macro,Auto respawn)
-// @homepage     http://www.legendmod.ml
+// @homepage     http://www.jimboy3100.github.io
 // @author       Jimboy3100
 // @license      MIT
 // @icon         https://jimboy3100.github.io/banners/CropedImage128.gif
