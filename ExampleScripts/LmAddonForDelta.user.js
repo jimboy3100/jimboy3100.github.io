1c7a1c34754dbffca22a78ed4a8d43d81e387638
kyriakidisdimitrios <kyriakidis.demetrios@gmail.com>
2025-12-23 02:50:44 +0200

    protobuf changes fIX 13

--- ExampleScripts/LmAddonForDelta.user.js
+++ ExampleScripts/LmAddonForDelta.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LmAddonForDelta
 // @namespace    Jimboy3100 LegendMod
-// @version      101.6
+// @version      101.8
 // @description  Imperial Overlord Elite: Integrated Flags, SNEZ Broadcaster (DM), Sovereign Join, Discord "Play" Trigger with Custom Skin Thumbnail.
 // @author       Jimboy3100
 // @icon         https://www.legendmod.ml/banners/icon48.png
