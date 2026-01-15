74fab25622643fc09cf4065266759d2aad17ce01
Juniordev-WEB <valdernandesjunior4874@gmail.com>
2026-01-15 18:33:32 -0300

    Delete Lc/Nc1.2.html directory

--- Lc/Nc1.2.html/menu.js
+++
@@ -1,8 +0,0 @@
-  const menuToggle = document.getElementById("menuToggle");
-  const menu = document.getElementById("menu");
-
-  if (menuToggle && menu) {
-    menuToggle.addEventListener("click", () => {
-      menu.classList.toggle("active");
-    });
-  }
