9f09189b6cf399ef0bfe4b5105f2285b09c5304a
Dimitrios Kyriakidis <kyriakidis.demetrios@gmail.com>
2026-03-14 10:24:49 +0200

    v3.340: LW opcodes in default case, no switch interference

--- ogario/ogario.v4.js
+++ ogario/ogario.v4.js
@@ -1,4 +1,4 @@
-window.OgVer = 3.338;
+window.OgVer = 3.340;
 /* Source script - test
 Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 This is part of the Legend mod project
