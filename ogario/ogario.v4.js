ae9afd5e93342e48bc55482b821a7d18b0f919cd
Dimitrios Kyriakidis <kyriakidis.demetrios@gmail.com>
2026-04-09 01:30:58 +0300

    v3.370: Garix fingerprint, bot cleanup, leaderboard fix

--- ogario/ogario.v4.js
+++ ogario/ogario.v4.js
@@ -1,4 +1,4 @@
-window.OgVer = 3.360;
+window.OgVer = 3.370;
 if (document.URL.includes('jimboy3100.github.io') || document.URL.includes('legendmod.ml') || document.URL.includes('expanding.land')) {
     window.legendModFromWebsite = true;
     if (document.URL.includes('expanding.land')) {
