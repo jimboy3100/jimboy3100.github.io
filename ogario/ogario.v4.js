6ede88a5bed25fcd192c8d0565ec8ead2321228c
Dimitrios Kyriakidis <kyriakidis.demetrios@gmail.com>
2026-04-13 17:48:28 +0300

    v3.402

--- ogario/ogario.v4.js
+++ ogario/ogario.v4.js
@@ -1,4 +1,4 @@
-window.OgVer = 3.401;
+window.OgVer = 3.402;
 if (document.URL.includes('jimboy3100.github.io') || document.URL.includes('legendmod.ml') || document.URL.includes('expanding.land')) {
     window.legendModFromWebsite = true;
     if (document.URL.includes('expanding.land')) {
