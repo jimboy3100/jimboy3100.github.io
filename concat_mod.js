b812afb54cb0500f699fa16b7ef5d3a773040a92
Dimitrios Kyriakidis <kyriakidis.demetrios@gmail.com>
2026-07-31 22:08:59 +0300

    update concat_mod.js for C server and discord bot garbadge bundles

--- concat_mod.js
+++ concat_mod.js
@@ -1,5 +1,13 @@
 const fs = require('fs');
-const files = [
+const path = require('path');
+
+const garbadgeDir = 'C:/Github_repos/garbadge';
+if (!fs.existsSync(garbadgeDir)) {
+  fs.mkdirSync(garbadgeDir, { recursive: true });
+}
+
+// --- 1. Client Code ---
+const clientFiles = [
   'C:/Github_repos/jimboy3100.github.io/ogario/ogario.v4.js',
   'C:/Github_repos/jimboy3100.github.io/LMexpress/LMexpress.js',
   'C:/Github_repos/jimboy3100.github.io/LMexpress/lm_extended_ui.js',
@@ -8,24 +16,70 @@ const files = [
   'C:/Github_repos/jimboy3100.github.io/extras/legendhelper.js',
   'C:/Github_repos/jimboy3100.github.io/play.html'
 ];
-let output = '';
-for (const file of files) {
+
+let clientOutput = '';
+for (const file of clientFiles) {
   if (fs.existsSync(file)) {
-    output += '\n/* --- ' + file + ' --- */\n';
-    output += fs.readFileSync(file, 'utf8');
+    clientOutput += '\n/* --- ' + file + ' --- */\n';
+    clientOutput += fs.readFileSync(file, 'utf8');
   } else {
     console.log('File not found: ' + file);
   }
 }
 
-if (!fs.existsSync('C:/Github_repos/garbadge')) {
-  fs.mkdirSync('C:/Github_repos/garbadge', { recursive: true });
+fs.writeFileSync(path.join(garbadgeDir, 'AllFiles'), clientOutput, 'utf8');
+fs.writeFileSync(path.join(garbadgeDir, 'AllFiles.txt'), clientOutput, 'utf8');
+fs.writeFileSync(path.join(garbadgeDir, 'AllFilesNew.txt'), clientOutput, 'utf8');
+fs.writeFileSync(path.join(garbadgeDir, 'LegendMod.txt'), clientOutput, 'utf8');
+fs.writeFileSync(path.join(garbadgeDir, 'LegendModNew.txt'), clientOutput, 'utf8');
+console.log('Generated Client Code bundles (AllFiles, LegendMod.txt, etc.)');
+
+// --- 2. C Server Code (LegendWorld) ---
+const serverSrcDir = 'C:/Github_repos/LegendWorld/src';
+let serverOutput = '';
+if (fs.existsSync(serverSrcDir)) {
+  const serverFiles = fs.readdirSync(serverSrcDir)
+    .filter(file => file.endsWith('.c') || file.endsWith('.h'))
+    .sort();
+
+  for (const file of serverFiles) {
+    const fullPath = path.join(serverSrcDir, file);
+    serverOutput += '\n/* --- ' + fullPath + ' --- */\n';
+    serverOutput += fs.readFileSync(fullPath, 'utf8');
+  }
 }
 
-fs.writeFileSync('C:/Github_repos/garbadge/AllFiles', output, 'utf8');
-fs.writeFileSync('C:/Github_repos/garbadge/AllFiles.txt', output, 'utf8');
-fs.writeFileSync('C:/Github_repos/garbadge/AllFilesNew.txt', output, 'utf8');
-fs.writeFileSync('C:/Github_repos/garbadge/LegendMod.txt', output, 'utf8');
-fs.writeFileSync('C:/Github_repos/garbadge/LegendModNew.txt', output, 'utf8');
+fs.writeFileSync(path.join(garbadgeDir, 'cServer.txt'), serverOutput, 'utf8');
+fs.writeFileSync(path.join(garbadgeDir, 'cServerNew.txt'), serverOutput, 'utf8');
+fs.writeFileSync(path.join(garbadgeDir, 'LegendWorld.txt'), serverOutput, 'utf8');
+fs.writeFileSync(path.join(garbadgeDir, 'LegendWorldNew.txt'), serverOutput, 'utf8');
+fs.writeFileSync(path.join(garbadgeDir, 'ServerCode.txt'), serverOutput, 'utf8');
+fs.writeFileSync(path.join(garbadgeDir, 'ServerCodeNew.txt'), serverOutput, 'utf8');
+console.log('Generated C Server Code bundles (cServer.txt, cServerNew.txt, LegendWorld.txt, etc.)');
 
-console.log('Successfully generated all garbadge files: AllFiles, AllFiles.txt, AllFilesNew.txt, LegendMod.txt, LegendModNew.txt');
+// --- 3. Discord Bot Code ---
+const botSrcDir = 'C:/Github_repos/discord-bot-server/src';
+let botOutput = '';
+if (fs.existsSync(botSrcDir)) {
+  const readmePath = 'C:/Github_repos/discord-bot-server/README.md';
+  if (fs.existsSync(readmePath)) {
+    botOutput += '\n/* --- ' + readmePath + ' --- */\n';
+    botOutput += fs.readFileSync(readmePath, 'utf8');
+  }
+
+  const botFiles = fs.readdirSync(botSrcDir)
+    .filter(file => file.endsWith('.js'))
+    .sort();
+
+  for (const file of botFiles) {
+    const fullPath = path.join(botSrcDir, file);
+    botOutput += '\n/* --- ' + fullPath + ' --- */\n';
+    botOutput += fs.readFileSync(fullPath, 'utf8');
+  }
+}
+
+fs.writeFileSync(path.join(garbadgeDir, 'DiscordBot.txt'), botOutput, 'utf8');
+fs.writeFileSync(path.join(garbadgeDir, 'DiscordBotNew.txt'), botOutput, 'utf8');
+fs.writeFileSync(path.join(garbadgeDir, 'DiscordBotServer.txt'), botOutput, 'utf8');
+fs.writeFileSync(path.join(garbadgeDir, 'DiscordBotServerNew.txt'), botOutput, 'utf8');
+console.log('Generated Discord Bot Code bundles (DiscordBot.txt, DiscordBotServer.txt, etc.)');
