52a8b904615d9596dcdbca947ca9b62c6c9b791d
Dimitrios Kyriakidis <kyriakidis.demetrios@gmail.com>
2026-07-31 22:05:43 +0300

    update concat_mod.js to generate all garbadge files

--- concat_mod.js
+++ concat_mod.js
@@ -5,6 +5,7 @@ const files = [
   'C:/Github_repos/jimboy3100.github.io/LMexpress/lm_extended_ui.js',
   'C:/Github_repos/jimboy3100.github.io/LMexpress/skinsAndDeals.js',
   'C:/Github_repos/jimboy3100.github.io/LMexpress/LMexpress.user.js',
+  'C:/Github_repos/jimboy3100.github.io/extras/legendhelper.js',
   'C:/Github_repos/jimboy3100.github.io/play.html'
 ];
 let output = '';
@@ -16,5 +17,15 @@ for (const file of files) {
     console.log('File not found: ' + file);
   }
 }
+
+if (!fs.existsSync('C:/Github_repos/garbadge')) {
+  fs.mkdirSync('C:/Github_repos/garbadge', { recursive: true });
+}
+
 fs.writeFileSync('C:/Github_repos/garbadge/AllFiles', output, 'utf8');
-console.log('Concatenated files to AllFiles');
+fs.writeFileSync('C:/Github_repos/garbadge/AllFiles.txt', output, 'utf8');
+fs.writeFileSync('C:/Github_repos/garbadge/AllFilesNew.txt', output, 'utf8');
+fs.writeFileSync('C:/Github_repos/garbadge/LegendMod.txt', output, 'utf8');
+fs.writeFileSync('C:/Github_repos/garbadge/LegendModNew.txt', output, 'utf8');
+
+console.log('Successfully generated all garbadge files: AllFiles, AllFiles.txt, AllFilesNew.txt, LegendMod.txt, LegendModNew.txt');
