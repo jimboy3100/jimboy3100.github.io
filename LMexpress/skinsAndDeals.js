56dd320ce1ef808dd56268b8742ba940a333688f
Dimitrios Kyriakidis <kyriakidis.demetrios@gmail.com>
2026-08-03 04:19:01 +0300

    Revert "fix: resolve overlapping Owned and Equipped badges in updateEquippedSkinUI"
    This reverts commit 42a7b52181c16503db3fabf7993654f41aaeb2f6.

--- LMexpress/skinsAndDeals.js
+++ LMexpress/skinsAndDeals.js
@@ -3392,46 +3392,15 @@ function updateEquippedSkinUI() {
     }
 
     // Highlight cards in the grid
-    $('.skin-card.equipped').each(function() {
-        var card = $(this);
-
-        card.removeClass('equipped');
-        card.find('.equipped-badge').remove();
-        card.find('.skin-btn-equip').text('Equip');
-
-        /*
-         * The previously equipped skin remains owned.
-         * Restore its normal Owned badge.
-         */
-        if (card.find('.owned-badge').length === 0) {
-            card.prepend(
-                '<div class="owned-badge">&#x2B50; Owned</div>'
-            );
-        }
-    });
-
+    $('.skin-card').removeClass('equipped');
+    $('.skin-card .equipped-badge').remove();
     $('.skin-card .skin-btn-equip').text('Equip');
 
     if (equippedId) {
         $('.skin-card[data-product-id="' + equippedId + '"]').each(function() {
-            var card = $(this);
-
-            /*
-             * Never display Owned and Equipped badges together.
-             */
-            card.find(
-                '.owned-badge, .equipped-badge'
-            ).remove();
-
-            card.addClass('equipped');
-
-            card.prepend(
-                '<div class="equipped-badge">&#x2714; Equipped</div>'
-            );
-
-            card.find(
-                '.skin-btn-equip'
-            ).text('Equipped');
+            $(this).addClass('equipped');
+            $(this).prepend('<div class="equipped-badge">&#x2714; Equipped</div>');
+            $(this).find('.skin-btn-equip').text('Equipped');
         });
     }
 }
