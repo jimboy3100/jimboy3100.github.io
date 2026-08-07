ad397ca9e911c4f29f7b899bff7c5d3dd55a4d1a
Dimitrios Kyriakidis <kyriakidis.demetrios@gmail.com>
2026-08-07 15:30:13 +0300

    feat: auto-hide nickname and mass for player cells when zoomed out

--- ogario/ogario.v4.js
+++ ogario/ogario.v4.js
@@ -18447,8 +18447,7 @@ function thelegendmodproject() {
                             //return;
                         }
                         else {
-                            var recursive = false;
-                            if (!(!this.isPlayerCell && (recursive = application.setAutoHideCellInfo(y)) && defaultmapsettings.autoHideNames && defaultmapsettings.autoHideMass)) {
+                            if (!((recursive = application.setAutoHideCellInfo(y)) && defaultmapsettings.autoHideNames && defaultmapsettings.autoHideMass)) {
                                 this.setDrawing();
                                 this.setDrawingScale();
                                 if (defaultSettings.textAlpha != 1) {
@@ -30256,11 +30255,8 @@ Most cells eaten   : ${mostCellsEaten}
             var viewScale = this.scale || 1;
 
             /* Match Canvas2D auto-hide: pass cell size (world units), same as Canvas2D's `y` */
-            var recursive = false;
-            if (!cell.isPlayerCell) {
-                recursive = application.setAutoHideCellInfo(cellSize);
-                if (recursive && defaultmapsettings.autoHideNames && defaultmapsettings.autoHideMass) return;
-            }
+            var recursive = application.setAutoHideCellInfo(cellSize);
+            if (recursive && defaultmapsettings.autoHideNames && defaultmapsettings.autoHideMass) return;
 
             /* No text to draw at all? */
             if (defaultmapsettings.noNames && !defaultmapsettings.showMass) return;
